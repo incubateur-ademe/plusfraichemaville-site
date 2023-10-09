02508fe1f05ff80ccd306cdf9de142aa87e4cdee
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-10-09 17:18:01 +0200

    Feat/add directus (#1)
    * Add directus SDK
    
    * Add directus token
    
    * Typo
    
    * Prettier
    
    * Fix build

---
+++ app/fiches-techniques/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { getFichesTechniques } from "@/lib/directus/queries/fichesTechniques";
+
+export default async function Posts() {
+  const allFichesTechniques = await getFichesTechniques();
+  return (
+    <>
+      <h1>Toutes les fiches techniques</h1>
+      {allFichesTechniques.map((ficheTechnique) => (
+        <div key={ficheTechnique.id}>
+          <Link href={`/fiches-techniques/${ficheTechnique.id}`}>{ficheTechnique.titre}</Link>
+        </div>
+      ))}
+      <Link href="/">Retour à l{`'`}accueil</Link>
+    </>
+  );
+}
---
+++ lib/directus/directusClient.ts
@@ -0,0 +1,12 @@
+import { createDirectus, staticToken } from "@directus/sdk";
+import { rest } from "@directus/sdk/rest";
+import { CustomDirectusTypes } from "@/lib/directus/directusModels";
+
+const DIRECTUS_URL = process.env.DIRECTUS_URL || "http://127.0.0.1/";
+const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN || "";
+export const DIRECTUS_ASSET_URL = DIRECTUS_URL + "/assets/";
+
+// Client with REST support
+export const directusClient = createDirectus<CustomDirectusTypes>(DIRECTUS_URL)
+  .with(rest())
+  .with(staticToken(DIRECTUS_TOKEN));
