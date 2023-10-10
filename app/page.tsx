5621372b97559c710125e6eb3af4c2443512b08a
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-10-10 16:28:54 +0200

    Use Directus API instead of SDK for better caching management

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Page d accueil</h1>
+      <h3>
+        <Link href="/fiches-techniques/">Voir la liste des fiches techniques du CMS</Link>
+      </h3>
     </main>
   );
 }
--- lib/directus/directusClient.ts
+++ lib/directus/directusClient.ts
@@ -1,12 +1,31 @@
-import { createDirectus, staticToken } from "@directus/sdk";
-import { rest } from "@directus/sdk/rest";
-import { CustomDirectusTypes } from "@/lib/directus/directusModels";
-
-const DIRECTUS_URL = process.env.DIRECTUS_URL || "http://127.0.0.1/";
-const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN || "";
+export const DIRECTUS_URL = process.env.DIRECTUS_URL || "http://127.0.0.1/";
+export const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN || "";
 export const DIRECTUS_ASSET_URL = DIRECTUS_URL + "/assets/";
 
-// Client with REST support
-export const directusClient = createDirectus<CustomDirectusTypes>(DIRECTUS_URL)
-  .with(rest())
-  .with(staticToken(DIRECTUS_TOKEN));
+export const directusGraphQLCall = async (query: String, variables?: any) => {
+  try {
+    return await fetch(DIRECTUS_URL + "/graphql", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${DIRECTUS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+      next: { revalidate: +(process.env.DIRECTUS_CACHE_TTL || 0) },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res.data && res.errors) {
+          console.log("API returned an error", res.errors);
+        } else {
+          return res.data;
+        }
+      })
+      .catch((err) => console.log("error in directusGraphQlCall ", err));
+  } catch (err) {
+    console.log("ERROR in directusGraphQlCall ", err);
+  }
+};
