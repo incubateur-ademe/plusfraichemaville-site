9faf13a6c17bf2c8654caec0b797a4aa8f17539c
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-11-06 11:02:53 +0100

    Fiche technique card UI

--- app/aide-decision/[aideDecisionEtapeSlug]/AideDecisionResult.tsx
+++ app/aide-decision/[aideDecisionEtapeSlug]/AideDecisionResult.tsx
@@ -15,7 +15,7 @@ export default async function AideDecisionResult({ aideDecisionEtapeSlug }: Prop
         <h1 className={"mb-4 text-center"}>Les solutions possibles</h1>
         <ul className="flex list-none flex-wrap justify-center p-0">
           {aideDecisionEtape.fiche_technique_id.map((ficheTechnique) => (
-            <li key={ficheTechnique.fiche_technique_id.id} className="m-2 w-72 flex">
+            <li key={ficheTechnique.fiche_technique_id.id} className="m-2 w-80 flex">
               <FicheTechniqueCardWithUserInfo
                 ficheTechnique={ficheTechnique.fiche_technique_id}
                 aideDecisionFirstStepName={(historique && historique[1].label) || ""}
--- components/ficheTechnique/FicheTechniqueFullCard.tsx
+++ components/ficheTechnique/FicheTechniqueFullCard.tsx
@@ -21,46 +21,50 @@ export default async function FicheTechniqueFullCard({ ficheTechnique }: { fiche
               {ficheTechnique.titre}
             </a>
           </h3>
-          <div className="fr-card__desc">
-            <div>{ficheTechnique.description_courte}</div>
-            <div className="mt-6 mb-2 text-right">
-              <div className="fr-text--bold text-4xl text-dsfr-text-color-green">-1,5 °</div>
-              <div className="text-xs text-dsfr-text-color-grey">Température locale</div>
-            </div>
-            <hr className="pb-1" />
-            <div className="text-dsfr-text-color-grey">
-              <div className="fr-text--bold text-xs text-dsfr-text-color-grey">Rapidité effective</div>
-              <div className="inline-block w-full">
-                <div className="float-left">
-                  <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
-                  <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
-                  <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
+          <div className="fr-card__desc h-full">
+            <div className={"flex justify-between flex-col h-full"}>
+              <div>{ficheTechnique.description_courte}</div>
+              <div>
+                <div className="mt-6 mb-2 text-right">
+                  <div className="fr-text--bold text-4xl text-dsfr-text-color-green">-1,5 °</div>
+                  <div className="text-xs text-dsfr-text-color-grey">Température locale</div>
                 </div>
-                <div className="float-right text-xs mt-1">de 6 mois à un an</div>
-              </div>
-              <hr className="pb-1" />
-              <div className="fr-text--bold text-xs">Pérénnité</div>
-              <div className="inline-block w-full">
-                <div className="float-left">
-                  <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
-                  <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
-                  <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
+                <hr className="pb-1" />
+                <div className="text-dsfr-text-color-grey">
+                  <div className="fr-text--bold text-xs text-dsfr-text-color-grey">Rapidité effective</div>
+                  <div className="inline-block w-full">
+                    <div className="float-left">
+                      <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
+                      <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
+                      <span className={"ri-hourglass-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
+                    </div>
+                    <div className="float-right text-xs mt-1">de 6 mois à un an</div>
+                  </div>
+                  <hr className="pb-1" />
+                  <div className="fr-text--bold text-xs">Pérénnité</div>
+                  <div className="inline-block w-full">
+                    <div className="float-left">
+                      <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
+                      <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-green"}></span>
+                      <span className={"ri-shining-2-fill fr-icon--sm text-dsfr-text-color-grey opacity-50"}></span>
+                    </div>
+                    <div className="float-right text-xs mt-1">de 6 mois à un an</div>
+                  </div>
+                  <hr className="pb-1" />
+                  <div className="fr-text--bold text-xs">Coût</div>
+                  <div className="inline-block w-full">
+                    <div className="float-left">
+                      <span className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-green"}></span>
+                      <span className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-green"}></span>
+                      <span
+                        className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-grey opacity-50"}
+                      ></span>
+                    </div>
+                    <div className="float-right text-xs mt-1">de 3000 € à 10 000 €</div>
+                  </div>
+                  <hr className="pb-1" />
                 </div>
-                <div className="float-right text-xs mt-1">de 6 mois à un an</div>
               </div>
-              <hr className="pb-1" />
-              <div className="fr-text--bold text-xs">Coût</div>
-              <div className="inline-block w-full">
-                <div className="float-left">
-                  <span className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-green"}></span>
-                  <span className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-green"}></span>
-                  <span
-                    className={"fr-icon-money-euro-circle-line fr-icon--sm text-dsfr-text-color-grey opacity-50"}
-                  ></span>
-                </div>
-                <div className="float-right text-xs mt-1">de 3000 € à 10 000 €</div>
-              </div>
-              <hr className="pb-1" />
             </div>
           </div>
         </div>
