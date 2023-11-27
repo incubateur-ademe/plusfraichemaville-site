ae3c87865808078259cedd1868d40e0e4b363492
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-11-27 14:30:03 +0100

    feat: finalize aide decision first step cards

--- app/aide-decision/page.tsx
+++ app/aide-decision/page.tsx
@@ -1,22 +1,23 @@
 // Has to put force-dynamic while https://github.com/vercel/next.js/issues/56018 is not fixed
-export const dynamic = "force-dynamic";
+import AideDecisionFirstStepEtapeCard from "@/components/aideDecision/AideDecisionFirstStepCard";
 import { getAideDecisionFirstSteps } from "@/lib/directus/queries/aideDecisionQueries";
-import AideDecisionEtapeCard from "@/components/aideDecision/aideDecisionEtapeCard";
-import { Breadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb";
+
+export const dynamic = "force-dynamic";
 
 export default async function Posts() {
   const aideDecisionFirstSteps = await getAideDecisionFirstSteps();
   return (
-    <>
-      <Breadcrumb currentPageLabel={"Choix de l'espace"} segments={[]} homeLinkProps={{ href: "/" }} />
-      <h1 className={"mb-4 text-center"}>Sur quel espace voulez-vous agir ?</h1>
-      <ul className="flex list-none flex-wrap justify-center p-0">
-        {aideDecisionFirstSteps.map((aideDecision) => (
-          <li key={aideDecision.id} className="m-2 w-80 flex">
-            <AideDecisionEtapeCard aideDecisionEtape={aideDecision} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className={"fr-container"}>
+      <h1 className={"mb-12 pt-14 text-center fr-h4"}>Sur quel espace voulez-vous agir ?</h1>
+      <div className="max-w-2xl m-auto">
+        <ul className="flex list-none flex-wrap justify-center pb-36">
+          {aideDecisionFirstSteps.map((aideDecision) => (
+            <li key={aideDecision.id} className="m-2 w-36 flex">
+              <AideDecisionFirstStepEtapeCard aideDecisionEtape={aideDecision} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
