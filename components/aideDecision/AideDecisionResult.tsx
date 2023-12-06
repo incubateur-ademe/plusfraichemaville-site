df1c08aa915c521d6bc5f0c8f8d99fa8b5a6b006
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-12-06 09:57:09 +0100

    style : add stepper in ad result page.

--- components/aideDecision/AideDecisionResult.tsx
+++ components/aideDecision/AideDecisionResult.tsx
@@ -2,6 +2,7 @@ import { getAideDecisionHistoryBySlug } from "@/lib/directus/queries/aideDecisio
 import { AideDecisionEtape } from "@/lib/directus/directusModels";
 import FicheSolutionCardWithUserInfo from "@/components/ficheSolution/FicheSolutionCardWithUserInfo";
 import FicheSolutionFullCard from "@/components/ficheSolution/FicheSolutionFullCard";
+import AideDecisionBreadcrumbs from "@/components/aideDecision/AideDecisionBreadcrumbs";
 
 type Props = {
   aideDecisionEtape: AideDecisionEtape;
@@ -12,19 +13,32 @@ export default async function AideDecisionResult({ aideDecisionEtape }: Props) {
   if (aideDecisionEtape.fiches_solutions.length > 0) {
     return (
       <div className={"fr-container"}>
-        <h1 className={"mb-4 pt-10 fr-h4"}>Découvrez les solutions proposées pour votre recherche</h1>
-        <ul className="flex list-none flex-wrap justify-center p-0">
-          {aideDecisionEtape.fiches_solutions.map((ficheSolution) => (
-            <li key={ficheSolution.fiche_solution_id.id} className="m-2 flex">
-              <FicheSolutionCardWithUserInfo
-                ficheSolution={ficheSolution.fiche_solution_id}
-                aideDecisionFirstStepName={(historique && historique[1].label) || ""}
-              >
-                <FicheSolutionFullCard ficheSolution={ficheSolution.fiche_solution_id} />
-              </FicheSolutionCardWithUserInfo>
-            </li>
-          ))}
-        </ul>
+        <div className="flex flex-row justify-items-center">
+          {historique && (
+            <AideDecisionBreadcrumbs
+              currentPageLabel={aideDecisionEtape.nom}
+              historique={historique}
+              className="hidden md:block"
+            />
+          )}
+          <div className="grow">
+            <h1 className={"mb-4 pt-10 fr-h4 text-center md:text-left"}>
+              Découvrez les solutions proposées pour votre recherche
+            </h1>
+            <ul className="flex list-none flex-wrap justify-center md:justify-start p-0">
+              {aideDecisionEtape.fiches_solutions.map((ficheSolution) => (
+                <li key={ficheSolution.fiche_solution_id.id} className="m-2 flex">
+                  <FicheSolutionCardWithUserInfo
+                    ficheSolution={ficheSolution.fiche_solution_id}
+                    aideDecisionFirstStepName={(historique && historique[1].label) || ""}
+                  >
+                    <FicheSolutionFullCard ficheSolution={ficheSolution.fiche_solution_id} />
+                  </FicheSolutionCardWithUserInfo>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   } else {
