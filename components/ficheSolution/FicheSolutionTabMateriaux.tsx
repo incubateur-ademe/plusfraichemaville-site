2ae8852071be8c42921db48282e1c34f0f55d200
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-12-14 13:45:56 +0100

    feat : materiaux tab in fiche solution page mobile version

--- components/ficheSolution/FicheSolutionTabMateriaux.tsx
+++ components/ficheSolution/FicheSolutionTabMateriaux.tsx
@@ -15,8 +15,8 @@ export default function FicheSolutionTabMateriaux({ ficheSolution }: { ficheSolu
           <hr className="p-0 h-[1px]" />
           {ficheSolution.materiaux.map(({ materiau_id: mat }) => (
             <div key={mat.id}>
-              <div className={"flex flex-row gap-6"}>
-                <div className="w-28 h-28 relative flex flex-none mt-8">
+              <div className={"flex flex-col md:flex-row gap-1 md:gap-6"}>
+                <div className="w-28 h-28 relative hidden md:flex flex-none mt-8">
                   <Image
                     fill
                     src={getDirectusImageUrl(mat.image, DIRECTUS_IMAGE_KEY_SIZE.ficheSolutionCard)}
@@ -24,12 +24,24 @@ export default function FicheSolutionTabMateriaux({ ficheSolution }: { ficheSolu
                     className={"object-cover rounded-2xl"}
                   />
                 </div>
-                <div className="mb-8 mt-8 text-dsfr-text-little-grey">
-                  <div className="text-2xl font-bold  mb-2">{mat.titre}</div>
+                <div className="mb-0 md:mb-8 mt-8 text-dsfr-text-little-grey">
+                  <div className="flex items-center gap-6 mb-4">
+                    <div className="w-28 h-28 relative flex md:hidden flex-none">
+                      <Image
+                        fill
+                        src={getDirectusImageUrl(mat.image, DIRECTUS_IMAGE_KEY_SIZE.ficheSolutionCard)}
+                        alt={mat.titre}
+                        className={"object-cover rounded-2xl"}
+                      />
+                    </div>
+                    <div className="text-2xl font-bold">{mat.titre}</div>
+                  </div>
                   <CmsRichText label={mat.description} className="text-sm" />
                 </div>
                 <div
-                  className={"w-60 flex flex-col flex-none bg-dsfr-background-alt-grey text-dsfr-text-mention-grey p-6"}
+                  className={
+                    "md:w-60 flex flex-col flex-none bg-dsfr-background-alt-grey text-dsfr-text-mention-grey p-6"
+                  }
                 >
                   <div>
                     <b>{`${mat.cout_minimum_fourniture} - ${mat.cout_maximum_fourniture} € `}</b>HT /{" "}
@@ -48,19 +60,24 @@ export default function FicheSolutionTabMateriaux({ ficheSolution }: { ficheSolu
       {ficheSolution.cout_minimum_entretien && ficheSolution.cout_maximum_entretien && (
         <>
           <hr className="p-0 h-[1px] mt-16" />
-          <div className={"flex flex-row gap-6"}>
-            <div className="w-28 h-28 relative flex flex-none mt-8 mb-8">
+          <div className={"flex flex-col md:flex-row gap-1 md:gap-6"}>
+            <div className="w-28 h-28 relative hidden md:flex flex-none mt-8 mb-8">
               <Image fill src={entretienIcon} alt="Coût d'entretien" />
             </div>
-            <div className="mb-8 mt-8 text-dsfr-text-little-grey flex flex-col grow">
-              <div className="text-2xl font-bold mb-2">{"Coût d'entretien"}</div>
+            <div className="mb-0 md:mb-8 mt-8 text-dsfr-text-little-grey flex flex-col grow">
+              <div className="flex items-center gap-6 mb-4">
+                <div className="w-28 h-28 relative flex md:hidden flex-none">
+                  <Image fill src={entretienIcon} alt="Coût d'entretien" />
+                </div>
+                <div className="text-2xl font-bold mb-2">{"Coût d'entretien"}</div>
+              </div>
               <div className="text-sm flex grow">
                 {"Main - d’œuvre ou sous-traitance, hors consommations d’eau et d’électricité"}
               </div>
             </div>
             <div
               className={
-                "w-60 flex flex-col flex-none bg-dsfr-background-alt-blue-france text-dsfr-text-mention-grey p-6"
+                "md:w-60 flex flex-col flex-none bg-dsfr-background-alt-blue-france text-dsfr-text-mention-grey p-6"
               }
             >
               <div>
