819d8a3ad720e3772b8608811da19af876b29e08
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-01-04 14:20:20 +0100

    chore : prettier

--- components/homepage/HomeImageSlider.tsx
+++ components/homepage/HomeImageSlider.tsx
@@ -3,9 +3,9 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
 
 const images = [
-  { original: "/images/homepage/carrousel1.jpg", originalAlt:"Image de rafraichissement urbain" },
-  { original: "/images/homepage/carrousel1.jpg", originalAlt:"Image de rafraichissement urbain" },
-  { original: "/images/homepage/carrousel1.jpg", originalAlt:"Image de rafraichissement urbain" },
+  { original: "/images/homepage/carrousel1.jpg", originalAlt: "Image de rafraichissement urbain" },
+  { original: "/images/homepage/carrousel1.jpg", originalAlt: "Image de rafraichissement urbain" },
+  { original: "/images/homepage/carrousel1.jpg", originalAlt: "Image de rafraichissement urbain" },
 ];
 
 export const HomeImageSlider = () => {
