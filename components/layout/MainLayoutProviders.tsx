6e97ba475beac13a2e1a2e0fbdd4d70d85a1cc95
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-01-11 19:51:37 +0100

    fix : remove useless searchparams

--- components/layout/MainLayoutProviders.tsx
+++ components/layout/MainLayoutProviders.tsx
@@ -1,8 +1,7 @@
 "use client";
-
-import useTrackPageView from "@/hooks/useTrackPageView";
 import { PropsWithChildren } from "react";
 import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
+import useTrackPageView from "@/hooks/useTrackPageView";
 
 export default function MainLayoutProviders({ children, lang }: PropsWithChildren<{ lang: string }>) {
   useTrackPageView();
--- hooks/useTrackPageView.ts
+++ hooks/useTrackPageView.ts
@@ -1,13 +1,10 @@
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { trackPageView } from "@/helpers/matomo/trackEvent";
 
 export default function useTrackPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   useEffect(() => {
-    // @ts-ignore
-    const url = `${pathname}${[...searchParams.keys()].length ? "?" + searchParams : ""}`;
-    trackPageView(url);
+    trackPageView(pathname);
   }, [pathname]);
 }
