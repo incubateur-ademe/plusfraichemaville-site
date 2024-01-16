17461e9b596a39caa0188fa9034a4a02e39f2595
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-01-16 11:25:13 +0100

    fix : fix double matomo call

--- components/matomo/MatomoScript.tsx
+++ components/matomo/MatomoScript.tsx
@@ -10,7 +10,6 @@ export default function MatomoScript() {
             {`
           var _paq = window._paq = window._paq || [];
           /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
             var u="${process.env.NEXT_PUBLIC_MATOMO_URL}";
