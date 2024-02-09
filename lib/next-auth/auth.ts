59eb47aaf338e8e2beadf10e2bbd48f7dfa6bede
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-02-09 23:44:18 +0100

    feat : working version of AgentConnect

--- lib/next-auth/auth.ts
+++ lib/next-auth/auth.ts
@@ -1,10 +1,11 @@
-import { getServerSession, NextAuthOptions, User } from "next-auth";
+import { getServerSession, NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import { PFMV_ROUTES } from "@/helpers/routes";
 import { v4 as uuidv4 } from "uuid";
+import * as Sentry from "@sentry/nextjs";
 
 const prisma = new PrismaClient();
 
@@ -16,35 +17,6 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt", // Use JSON Web Tokens (JWT) for session management
   },
-  callbacks: {
-    async session({ session, token }: { session: any; token: any }) {
-      console.log("session", session);
-      console.log("token", token);
-      console.log("session.user", session.user);
-      // const databaseuser = await prisma.user.findUnique({ where: { email: token.email } });
-      // console.log("user in session", databaseuser);
-      return { ...session, user: { ...session.user} };
-    },
-    async jwt({ token, user }: { token: any; user: any }) {
-      console.log("token", token);
-      console.log("user", user);
-      // const dbUser = await prisma.utilisateur.findFirst({ where: { email: token.email } });
-      // console.log("dbUser", dbUser);
-
-      return token;
-    },
-  },
-  events: {
-    createUser: (message: { user: User }) => {
-      console.log("message", message);
-      console.log("user", message.user);
-      prisma.utilisateur.create({
-        data: {
-          email: message.user.email,
-        },
-      });
-    },
-  },
   pages: {
     signIn: PFMV_ROUTES.CONNEXION,
   },
@@ -53,30 +25,53 @@ export const authOptions: NextAuthOptions = {
       id: "agentconnect",
       name: "Agent Connect",
       type: "oauth",
-      wellKnown: process.env.AGENT_CONNECT_WELLKNOWN_URL,
+      idToken: true,
+      clientId: process.env.AGENT_CONNECT_ID,
+      clientSecret: process.env.AGENT_CONNECT_SECRET,
+      wellKnown: process.env.AGENT_CONNECT_BASE_URL + "/v2/.well-known/openid-configuration",
+      allowDangerousEmailAccountLinking: true,
       checks: ["nonce", "state"],
       authorization: {
         params: {
-          scope: "openid uid given_name usual_name email",
+          scope: "openid profile uid given_name usual_name email siren",
           acr_values: "eidas1",
           redirect_uri: process.env.NEXT_PUBLIC_URL_SITE + "/api/auth/callback/agentconnect",
           nonce: uuidv4(),
           state: uuidv4(),
         },
       },
-      idToken: true,
-      clientId: process.env.AGENT_CONNECT_ID,
-      clientSecret: process.env.AGENT_CONNECT_SECRET,
       client: {
         authorization_signed_response_alg: "HS256",
         id_token_signed_response_alg: "HS256",
+        userinfo_encrypted_response_alg: "HS256",
+        userinfo_signed_response_alg: "HS256",
+        userinfo_encrypted_response_enc: "HS256",
       },
-      profile(profile) {
-        // return profile;
-        console.log("profile", profile)
+      userinfo: {
+        async request(context) {
+          try {
+            const userInfo = await fetch(process.env.AGENT_CONNECT_BASE_URL + "/v2/userinfo", {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${context.tokens.access_token}`,
+              },
+            }).then((res) => {
+              return res.text();
+            });
+            return JSON.parse(Buffer.from(userInfo.split(".")[1], "base64").toString());
+          } catch (err: any) {
+            console.log("Error while getting info from AgentConnect", err);
+            Sentry.captureException(err);
+            throw new Error(err);
+          }
+        },
+      },
+      profile: async (profile) => {
         return {
           id: profile.sub,
-          name: profile.name,
+          name: `${profile.given_name} ${profile.usual_name}`,
+          prenom: profile.given_name,
+          nom: profile.usual_name,
           email: profile.email,
         };
       },
