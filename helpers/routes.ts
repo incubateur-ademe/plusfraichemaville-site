7534ece2232d174087b883813552ddac449a6b30
mehdilouraoui <med.louraoui@gmail.com>
2024-02-27 22:32:34 +0100

    feat: add a route helper and fetch wrapper for dx

--- helpers/routes.ts
+++ helpers/routes.ts
@@ -6,4 +6,27 @@ export const PFMV_ROUTES = {
   DECONNEXION: "/logout",
   DECONNEXION_AGENT_CONNECT: "/logout/agentconnect",
   ESPACE_PROJET: "/espace-projet",
+  INFO_PROJET: "/espace-projet/info-projet",
+  LISTE_PROJETS: "/espace-projet/liste-projets",
+};
+
+export const PFMV_ROUTES_API = {
+  SEED_DUMMY_DATA: "/api/seed-dummy-data",
+  DELETE_PROJET: "/api/delete-projet",
+};
+
+type FetchURLType = keyof typeof PFMV_ROUTES_API;
+
+const config = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export const apiFetcher = (url: FetchURLType, body: {} = {}) => {
+  return fetch(PFMV_ROUTES_API[url], {
+    ...config,
+    body: body ? JSON.stringify(body) : undefined,
+  });
 };
