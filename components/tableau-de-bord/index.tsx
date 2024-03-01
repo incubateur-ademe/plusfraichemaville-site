05da9c50717c496504de85d7205af2e24f90d605
mehdilouraoui <med.louraoui@gmail.com>
2024-03-01 10:42:49 +0100

    chore: remove comments

--- components/tableau-de-bord/index.tsx
+++ components/tableau-de-bord/index.tsx
@@ -4,6 +4,3 @@ export { TableauDeBordTab } from "./tableau-de-bord-tab";
 export { TableauDeBordSuivi } from "./tableau-de-bord-suivi";
 export { TableauDeBordSuiviCard } from "./tableau-de-bord-suivi-card";
 export { TableauDeBordSuiviCardProgress } from "./tableau-de-bord-suivi-card-progress";
-// export { TableauDeBordSuiviWithFichesSolutions } from "./tableau-de-bord-suivi-card-with-fiches-solutions";
-// export { TableauDeBordSuiviCardWithList } from "./tableau-de-bord-suivi-card-with-list";
-// export { TableauDeBordSuiviWithText } from "./tableau-de-bord-suivi-card-with-text";
