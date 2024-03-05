169e38d5806305e7113ba0fedc4545d6e92cef89
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-03-05 14:25:15 +0100

    refacto: pr review

--- app/(private)/espace-projet/[projetId]/layout.tsx
+++ app/(private)/espace-projet/[projetId]/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { ReactElement } from "react";
+import { PropsWithChildren } from "react";
 import { useParams } from "next/navigation";
 import { useProjetsStore } from "@/stores/projets/provider";
 import BannerProjet from "@/components/monEspaceProjet/banner-projet";
 
-export default function Layout({ children }: { children: ReactElement | null }) {
+export default function Layout({ children }: PropsWithChildren) {
   const { projetId } = useParams();
   const setCurrentProjetId = useProjetsStore((state) => state.setCurrentProjetId);
   setCurrentProjetId(+projetId);
