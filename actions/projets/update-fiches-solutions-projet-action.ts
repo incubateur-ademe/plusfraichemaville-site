6cd5f90bcc2048a8dfa93e93c5a6e2f5ea0d86be
mehdilouraoui <med.louraoui@gmail.com>
2024-03-11 18:56:57 +0100

    fix: commit

--- actions/projets/update-fiches-solutions-projet-action.ts
+++ actions/projets/update-fiches-solutions-projet-action.ts
@@ -21,8 +21,5 @@ export const updateFichesSolutionsProjetAction = async (
 
   const projet = await updateFichesSolutionsProjet(projetId, fichesSolutionsId);
 
-
-  revalidatePath(PFMV_ROUTES.ESPACE_PROJET_FICHES_SOLUTIONS_LISTE(projetId));
-
   return { type: "success", message: "FICHE_SOLUTION_ADDED_TO_PROJET", projet };
 };
