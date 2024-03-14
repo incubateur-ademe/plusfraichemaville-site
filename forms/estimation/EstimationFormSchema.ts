f999ceb305e32fa281ce355d1e5c0991e515e1cf
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-03-14 17:16:17 +0100

    fix: should not allow estimation without fiche solution.

--- forms/estimation/EstimationFormSchema.ts
+++ forms/estimation/EstimationFormSchema.ts
@@ -1,4 +1,6 @@
 import z from "zod";
 
-export const EstimationFormSchema = z.object({ ficheSolutionIds: z.string().array() });
+export const EstimationFormSchema = z.object({
+  ficheSolutionIds: z.string().array().min(1, { message: "Vous devez sélectionner au moins une fiche solution" }),
+});
 export type EstimationFormData = z.infer<typeof EstimationFormSchema>;
