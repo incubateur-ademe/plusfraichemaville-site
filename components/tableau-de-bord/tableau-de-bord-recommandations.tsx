a6f30729fb5c9dbae31da66a4aaac67914d040fb
mehdilouraoui <med.louraoui@gmail.com>
2024-03-14 12:32:08 +0100

    fix: get unique fiches complementaires in reco

--- components/tableau-de-bord/tableau-de-bord-recommandations.tsx
+++ components/tableau-de-bord/tableau-de-bord-recommandations.tsx
@@ -3,9 +3,22 @@ import { getFicheSolutionById } from "@/lib/strapi/queries/fichesSolutionsQuerie
 import FicheSolutionCardWithUserInfo from "../ficheSolution/FicheSolutionCardWithUserInfo";
 
 import useSWRImmutable from "swr/immutable";
+import { useEffect, useMemo, useState } from "react";
+import { FicheSolutionResponse } from "../ficheSolution/type";
+
+const getUniqueFSComplementaires = (fichesSolutionsComplementaires: FicheSolutionResponse[]) =>
+  Object.values(
+    fichesSolutionsComplementaires.reduce<Record<string, FicheSolutionResponse>>((acc, item) => {
+      acc[item.id] = item;
+      return acc;
+    }, {}),
+  );
 
 export const TableauDeBordRecommandation = () => {
   const projet = useProjetsStore((state) => state.getCurrentProjet());
+  const [fsComplementaires, setFSComplementaires] = useState<FicheSolutionResponse[]>([]);
+  const updateFSComplementaires = (updates: FicheSolutionResponse[]) => setFSComplementaires(updates);
+  const fichesSolutionsUniques = useMemo(() => getUniqueFSComplementaires(fsComplementaires), [fsComplementaires]);
 
   if (!projet) {
     return null;
@@ -19,19 +32,37 @@ export const TableauDeBordRecommandation = () => {
         des solutions complémentaires à celles que vous avez choisies afin de créer les meilleures combinaisons et
         synergies possibles.
       </p>
+      {fichesSolutionsUniques.map((fs) => (
+        <FicheSolutionCardWithUserInfo ficheSolution={fs} key={fs.id} projectName="" />
+      ))}
       {projet?.fiches_solutions_id.map((ficheSolutionId) => (
-        <TableauDeBordRecommandationItem ficheSolutionId={ficheSolutionId} key={ficheSolutionId} />
+        <TableauDeBordRecommandationItem
+          setFSComplementaires={updateFSComplementaires}
+          ficheSolutionId={ficheSolutionId}
+          key={ficheSolutionId}
+        />
       ))}
     </div>
   );
 };
 
-const TableauDeBordRecommandationItem = ({ ficheSolutionId }: { ficheSolutionId: number }) => {
+const TableauDeBordRecommandationItem = ({
+  ficheSolutionId,
+  setFSComplementaires,
+}: {
+  ficheSolutionId: number;
+  setFSComplementaires: (_updates: FicheSolutionResponse[]) => void;
+}) => {
   const { data } = useSWRImmutable(`ficheSolution-${ficheSolutionId}`, () =>
     getFicheSolutionById(ficheSolutionId.toString()),
   );
 
-  return data?.attributes.fiches_solutions_complementaires?.data.map((fs) => (
-    <FicheSolutionCardWithUserInfo ficheSolution={fs} key={fs.id} projectName="" />
-  ));
+  useEffect(() => {
+    if (data && data.attributes.fiches_solutions_complementaires) {
+      const arr = data.attributes.fiches_solutions_complementaires.data;
+      setFSComplementaires(arr);
+    }
+  }, [data, setFSComplementaires]);
+
+  return null;
 };
