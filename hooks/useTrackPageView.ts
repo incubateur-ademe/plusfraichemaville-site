76de6c73751b868ed17a245e03a558046bd777be
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-03-18 17:54:00 +0100

    fix: fix double call + campaign in Matomo

--- hooks/useTrackPageView.ts
+++ hooks/useTrackPageView.ts
@@ -1,10 +1,12 @@
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { trackPageView } from "@/helpers/matomo/trackEvent";
 
 export default function useTrackPageView() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   useEffect(() => {
-    trackPageView(pathname);
-  }, [pathname]);
+    const url = `${pathname}${Array.from(searchParams.keys()).length ? "?" + searchParams : ""}`;
+    trackPageView(url);
+  }, [pathname, searchParams]);
 }
