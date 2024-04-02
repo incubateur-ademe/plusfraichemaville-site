94dfceb7ffc2d17ba8a7c07b42a0955bdfad88da
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-02 14:02:50 +0200

    feat: Add popup when saving fiche diagnostic

--- components/fiches-diagnostic/fiche-diagnostic-save-button.tsx
+++ components/fiches-diagnostic/fiche-diagnostic-save-button.tsx
@@ -3,6 +3,9 @@
 import { useUserStore } from "@/stores/user/provider";
 import Button from "@codegouvfr/react-dsfr/Button";
 import clsx from "clsx";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
+import Link from "next/link";
+import { PFMV_ROUTES } from "@/helpers/routes";
 
 type FicheDiagnosticSaveButtonProps = {
   ficheDiagnosticId: number;
@@ -10,6 +13,11 @@ type FicheDiagnosticSaveButtonProps = {
   showLabel?: boolean;
   className?: string;
 };
+
+const modal = createModal({
+  id: "bookmark-diag-modal",
+  isOpenedByDefault: false,
+});
 
 export const FicheDiagnosticSaveButton = ({
   ficheDiagnosticId,
@@ -19,22 +27,47 @@ export const FicheDiagnosticSaveButton = ({
 }: FicheDiagnosticSaveButtonProps) => {
   const updateBookmarkedFichesDiagnostic = useUserStore((state) => state.updateBookmarkedFichesDiagnostic);
 
-  const ficheDiagnosticIds = useUserStore((state) => state.userInfos?.selection_fiches_diagnostic);
-  const isSaved = ficheDiagnosticIds && ficheDiagnosticIds.includes(+ficheDiagnosticId);
+  const ficheDiagnosticIdsFromDatabase = useUserStore((state) => state.userInfos?.selection_fiches_diagnostic);
+  const isSavedInDatabase =
+    ficheDiagnosticIdsFromDatabase && ficheDiagnosticIdsFromDatabase.includes(+ficheDiagnosticId);
 
   const ficheDiagnosticIdsFromStorage = useUserStore((state) => state.bookmarkedFichesDiagnostic);
   const isSavedInStorage =
     ficheDiagnosticIdsFromStorage && ficheDiagnosticIdsFromStorage.includes(ficheDiagnosticId.toString());
 
-  const projetPicto = isSaved ? "ri-add-circle-fill" : "ri-add-circle-line";
-  const label = isSavedInStorage || isSaved ? "Sauvegardée" : showLabel ? "Sauvegarder" : "";
-  const userBookmarkPicto = isSavedInStorage || isSaved ? "fr-icon-bookmark-fill" : "fr-icon-bookmark-line";
+  const projetPicto = isSavedInDatabase ? "ri-add-circle-fill" : "ri-add-circle-line";
+  const label = isSavedInStorage || isSavedInDatabase ? "Sauvegardée" : showLabel ? "Sauvegarder" : "";
+  const userBookmarkPicto = isSavedInStorage || isSavedInDatabase ? "fr-icon-bookmark-fill" : "fr-icon-bookmark-line";
   const picto = projetId ? projetPicto : userBookmarkPicto;
 
-  const updateFicheDiagnostic = async () => updateBookmarkedFichesDiagnostic(ficheDiagnosticId);
+  const updateFicheDiagnostic = async () => {
+    updateBookmarkedFichesDiagnostic(ficheDiagnosticId);
+    if (!(isSavedInDatabase || isSavedInStorage)) {
+      modal.open();
+    }
+  };
 
   return (
     <>
+      <modal.Component title="" size="large">
+        <div className="flex items-center mb-10">
+          <i className={"fr-icon--lg fr-icon-arrow-right-line mr-4"} />
+          <span className="text-2xl font-bold">Méthode diagnostic ajoutée dans ma sélection</span>
+        </div>
+        <Button
+          priority="primary"
+          className="rounded-3xl !min-h-fit !text-sm md:ml-20 mr-4 mb-4"
+          onClick={() => modal.close()}
+        >
+          Continuer ma lecture
+        </Button>
+        <Link
+          href={PFMV_ROUTES.MES_FICHES_SOLUTIONS}
+          className="fr-btn fr-btn--secondary rounded-3xl !min-h-fit !text-sm mr-4"
+        >
+          Voir mes méthodes de diagnostic
+        </Link>
+      </modal.Component>
       {label ? (
         <Button
           onClick={updateFicheDiagnostic}
