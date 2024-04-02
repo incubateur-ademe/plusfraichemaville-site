cf21af3b2a0f9206f0d2fcbb3cd05d0f9d23d2d6
mehdilouraoui <med.louraoui@gmail.com>
2024-04-02 09:35:13 +0200

    fix

--- components/layout/MainLayoutProviders.tsx
+++ components/layout/MainLayoutProviders.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { PropsWithChildren } from "react";
 import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
-// import useTrackPageView from "@/hooks/useTrackPageView";
+import useTrackPageView from "@/hooks/useTrackPageView";
 import { SessionProvider } from "next-auth/react";
 import { ProjetsStoreProvider } from "@/stores/projets/provider";
 import { UserStoreProvider } from "@/stores/user/provider";
 
 export default function MainLayoutProviders({ children, lang }: PropsWithChildren<{ lang: string }>) {
-  // useTrackPageView();
+  useTrackPageView();
 
   return (
     <SessionProvider>
