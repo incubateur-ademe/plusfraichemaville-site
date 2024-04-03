be1185e65a4031991dac7fd7c1519de80b253019
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-03 11:52:03 +0200

    feat: add odd panel on new homepage

--- components/homepage/homepage-inspirer-card.tsx
+++ components/homepage/homepage-inspirer-card.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import RetourExperienceCard, { RexInHome } from "@/components/retourExperience/RetourExperienceCard";
 import { PFMV_ROUTES } from "@/helpers/routes";
 import clsx from "clsx";
