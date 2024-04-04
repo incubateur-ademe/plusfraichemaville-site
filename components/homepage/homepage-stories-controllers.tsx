09265eb845c119f277d3ebfa244f0c26040bf20c
mehdilouraoui <med.louraoui@gmail.com>
2024-04-04 09:57:21 +0200

    fix: fix button rotate slide

--- components/homepage/homepage-stories-controllers.tsx
+++ components/homepage/homepage-stories-controllers.tsx
@@ -18,7 +18,8 @@ export const HomepageStoriesController = ({ arrow }: { arrow: "right" | "left" }
         "group splide__arrows w-14 h-14 flex justify-center items-center rounded-full bg-black",
         arrow === "left" ? "splide__arrow splide__arrow--prev" : "splide__arrow splide__arrow--next",
         "cursor-pointer hover:bg-white hover:text-black transition-[background] duration-300",
-        "absolute top-1/2 hidden lg:block -translate-y-1/2",
+        "absolute top-1/2 hidden lg:block -translate-y-1/2 ",
+        arrow === "left" && "rotate-180",
         { [`${arrow}-6`]: arrow },
       )}
     >
