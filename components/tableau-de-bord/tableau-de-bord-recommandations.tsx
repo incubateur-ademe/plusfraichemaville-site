032bfeab43cf064e54b0b997dbc701515328942a
mehdilouraoui <med.louraoui@gmail.com>
2024-04-18 12:05:40 +0200

    fix: create skeleton file and remove useless animation

--- components/tableau-de-bord/tableau-de-bord-recommandations.tsx
+++ components/tableau-de-bord/tableau-de-bord-recommandations.tsx
@@ -5,6 +5,7 @@ import FicheSolutionCardWithUserInfo from "../ficheSolution/FicheSolutionCardWit
 import useSWR from "swr";
 
 import { FicheSolutionResponse } from "../ficheSolution/type";
+import { FicheSolutionSkeleton } from "../ficheSolution/fiche-solution-skeleton";
 
 export const TableauDeBordRecommandation = () => {
   const projet = useProjetsStore((state) => state.getCurrentProjet());
@@ -45,31 +46,15 @@ export const TableauDeBordRecommandation = () => {
       </p>
       {isLoading ? (
         <div className="flex gap-8">
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
+          <FicheSolutionSkeleton />
+          <FicheSolutionSkeleton />
+          <FicheSolutionSkeleton />
         </div>
       ) : (
         filteredFichesSolutionsComplementaires?.map(
           (fs) => fs && <FicheSolutionCardWithUserInfo ficheSolution={fs} key={fs?.id} projectName="" withoutModal />,
         )
       )}
-    </div>
-  );
-};
-
-const Skeleton = () => {
-  return (
-    <div className="w-72 pfmv-card h-[645px] bg-white">
-      <div className="animate-pulse">
-        <div className="bg-[#eee] rounded-t-xl h-44 w-full"></div>
-
-        <div className="bg-[rgb(226_232_240)] w-4/5 mx-auto h-3 rounded-xl mt-10"></div>
-        <div className="bg-[rgb(226_232_240)] w-4/5 mx-auto h-3 rounded-xl mt-4"></div>
-        <div className="bg-[rgb(226_232_240)] w-4/5 mx-auto h-3 rounded-xl mt-4"></div>
-        <div className="bg-[rgb(226_232_240)] w-4/5 mx-auto h-3 rounded-xl mt-4"></div>
-        <div className="bg-[rgb(226_232_240)] w-4/5 mx-auto h-3 rounded-xl mt-4"></div>
-      </div>
     </div>
   );
 };
