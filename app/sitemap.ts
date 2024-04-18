82129cb07b5d83be9496824e94d97cd39367468e
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-18 16:34:25 +0200

    fix: clean code

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,35 +13,35 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const staticUrls = staticPages.map((page) => ({
     url: `${baseUrl}/${page}`,
-    changeFrequency: "monthly" as "monthly",
+    changeFrequency: "monthly" as const,
     lastModified: new Date(),
     priority: 0.5,
   }));
 
   const importantUrls = importantPages.map((page) => ({
     url: `${baseUrl}/${page}`,
-    changeFrequency: "weekly" as "weekly",
+    changeFrequency: "weekly" as const,
     lastModified: new Date(),
     priority: 1.0,
   }));
 
   const ficheSolutionPages = allFichesSolutions.map((page) => ({
     url: `${baseUrl}/fiche-solution/${page.attributes.slug}`,
-    changeFrequency: "weekly" as "weekly",
+    changeFrequency: "weekly" as const,
     lastModified: new Date(),
     priority: 0.8,
   }));
 
   const ficheDiagnoscticPages = allFichesDiagnostics.map((page) => ({
     url: `${baseUrl}/fiches-diagnostic/${page.attributes.slug}`,
-    changeFrequency: "weekly" as "weekly",
+    changeFrequency: "weekly" as const,
     lastModified: new Date(),
     priority: 0.8,
   }));
 
   const retourExperiencePages = allRetourExperiences.map((page) => ({
     url: `${baseUrl}/projet/${page.attributes.slug}`,
-    changeFrequency: "weekly" as "weekly",
+    changeFrequency: "weekly" as const,
     lastModified: new Date(),
     priority: 0.8,
   }));
