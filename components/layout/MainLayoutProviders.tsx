e627477a11a3035d2a99a9dced0e8f851685b914
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-19 13:19:14 +0200

    feat: Set Matomo tracker in Suspense to enable SSG

--- components/layout/MainLayoutProviders.tsx
+++ components/layout/MainLayoutProviders.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
-import useTrackPageView from "@/hooks/useTrackPageView";
 import { SessionProvider } from "next-auth/react";
 import { ProjetsStoreProvider } from "@/stores/projets/provider";
 import { UserStoreProvider } from "@/stores/user/provider";
+import MatomoPageView from "@/components/matomo/matomo-page-view";
 
 export default function MainLayoutProviders({ children, lang }: PropsWithChildren<{ lang: string }>) {
-  useTrackPageView();
-
   return (
     <SessionProvider>
       <UserStoreProvider>
         <ProjetsStoreProvider>
+          <Suspense>
+            <MatomoPageView />
+          </Suspense>
           <DsfrProvider lang={lang}>{children}</DsfrProvider>
         </ProjetsStoreProvider>
       </UserStoreProvider>
--- components/matomo/matomo-page-view.tsx
+++ components/matomo/matomo-page-view.tsx
@@ -1,14 +1,15 @@
 "use client";
-
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { trackPageView } from "@/helpers/matomo/trackEvent";
 
-export default function useTrackPageView() {
+export default function MatomoPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   useEffect(() => {
     const url = `${pathname}${Array.from(searchParams.keys()).length ? "?" + searchParams : ""}`;
     trackPageView(url);
   }, [pathname, searchParams]);
+
+  return <></>;
 }
