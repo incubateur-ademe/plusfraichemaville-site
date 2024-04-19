9792d5bc5519f5c2b9473ef9862a9337eaca3302
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-19 13:55:09 +0200

    style: Add hover color on add and share icon

--- components/common/button-share-current-url.tsx
+++ components/common/button-share-current-url.tsx
@@ -25,8 +25,13 @@ export default function ButtonShareCurrentUrl({ className }: { className?: strin
             className="flex flex-row items-center gap-2 mb-4 text-dsfr-text-mention-grey cursor-pointer"
             onClick={copyCurrentUrlToClipboard}
           >
-            <div className="bg-pfmv-navy w-8 h-8 rounded-full flex items-center justify-center">
-              <i className={clsx("fr-icon--sm ri-share-forward-fill text-dsfr-text-label-blue-france text-white")} />
+            <div
+              className={clsx(
+                "w-8 h-8 rounded-full flex items-center justify-center",
+                "bg-pfmv-navy hover:!bg-dsfr-background-action-high-blue-france-active",
+              )}
+            >
+              <i className={clsx("fr-icon--sm ri-share-forward-fill text-white")} />
             </div>
             <span className="mt-[1px] text-dsfr-text-label-blue-france text-sm font-bold">Partager</span>
           </div>
@@ -34,7 +39,7 @@ export default function ButtonShareCurrentUrl({ className }: { className?: strin
       ) : (
         <div className="flex flex-row items-center gap-2 mb-4 text-dsfr-text-mention-grey">
           <div className="bg-pfmv-navy w-8 h-8 rounded-full flex items-center justify-center">
-            <i className={clsx(" fr-icon--sm ri-share-forward-fill text-dsfr-text-label-blue-france text-white")} />
+            <i className={clsx(" fr-icon--sm ri-share-forward-fill text-white")} />
           </div>
           <span className="mt-[1px] text-dsfr-text-label-blue-france text-sm font-bold">Partager</span>
         </div>
