0cf4e9dd62f1cdde351fa11504392684e0f76e3d
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-04-23 11:48:45 +0200

    chore: remove sts value as it is added by scalingo

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
           },
           {
             key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
+            value: "includeSubDomains; preload",
           },
           {
             key: "X-Frame-Options",
