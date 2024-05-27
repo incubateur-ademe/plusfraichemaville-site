43362037666cbfd118dd7e145484eeef01b67372
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-05-27 11:36:04 +0200

    feat: add iframe to metabase stat page

--- components/layout/AppFooter.tsx
+++ components/layout/AppFooter.tsx
@@ -72,6 +72,11 @@ export default function AppFooter() {
               </a>
             </li>
             <li className="fr-footer__bottom-item">
+              <a className="fr-footer__bottom-link" href="/stats" target="_self">
+                Statistiques
+              </a>
+            </li>
+            <li className="fr-footer__bottom-item">
               <a
                 className="fr-footer__bottom-link"
                 href="https://github.com/incubateur-ademe/plusfraichemaville-site"
