a12e83bdbe2756ac722e06a9f1afc852b04411c4
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-05-27 10:05:53 +0200

    feat: add legal notice for Climadiag

--- components/climadiag/climadiag-indicateurs.tsx
+++ components/climadiag/climadiag-indicateurs.tsx
@@ -7,6 +7,9 @@ export const ClimadiagIndicateurs = ({ climadiagInfo }: { climadiagInfo: Climadi
   return (
     <div className="bg-dsfr-background-open-blue-france">
       <ClimadiagIndicateursTabs data={climadiagInfo} />
+      <div className="float-right text-xs text-pfmv-grey">
+        Les données Climadiag sont la propriété exclusive de Météo-France.
+      </div>
     </div>
   );
 };
