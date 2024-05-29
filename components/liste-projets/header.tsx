5f63e757994634afc0599932d2b525aa7596a626
mehdilouraoui <med.louraoui@gmail.com>
2024-05-29 14:42:41 +0200

    fix disabled create project button

--- components/liste-projets/header.tsx
+++ components/liste-projets/header.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { ListProjetsHeaderEmpty } from "./empty";
+import { PFMV_ROUTES } from "@/helpers/routes";
 
 export const ListeProjetsHeader = ({ isListEmpty }: { isListEmpty: boolean }) => {
   return (
@@ -8,6 +10,11 @@ export const ListeProjetsHeader = ({ isListEmpty }: { isListEmpty: boolean }) =>
           <h2 className="text-dsfr-text-label-blue-france text-2xl mb-1">Mon espace projet</h2>
           <span className="text-lg block mb-8">Les projets de rafraîchissement de ma collectivité</span>
           {isListEmpty && <ListProjetsHeaderEmpty />}
+        </div>
+        <div className="shrink-0">
+          <Link href={PFMV_ROUTES.CREATE_PROJET} className="fr-btn ri-add-circle-fill fr-btn--icon-left rounded-3xl">
+            Créer un projet
+          </Link>
         </div>
       </div>
     </>
