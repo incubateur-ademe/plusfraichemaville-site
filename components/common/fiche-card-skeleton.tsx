cb513884803b53bfa25230eb966fb4531ba74042
mehdilouraoui <med.louraoui@gmail.com>
2024-05-29 15:54:27 +0200

    change fiche solution skeleton for global fiche skeleton and add skeleton to fiche diag

--- components/common/fiche-card-skeleton.tsx
+++ components/common/fiche-card-skeleton.tsx
@@ -1,41 +1,53 @@
 import clsx from "clsx";
 
-export const FicheCardSkeleton = ({ small }: { small?: boolean }) => {
+export const FicheCardSkeleton = ({ vertical }: { vertical?: boolean }) => {
   return (
-    <div className={clsx("w-72 pfmv-card h-[40.3125rem] bg-white", small ? "h-[26rem]" : "h-[40.3125rem]")}>
-      <div className="animate-pulse">
-        <div className="bg-dsfr-contrast-grey rounded-t-xl h-52 w-full"></div>
-
+    <div
+      className={clsx(
+        "pfmv-card  bg-white",
+        vertical ? "h-52 lg:w-full lg:max-w-[53rem]" : "h-[40.3125rem] w-72",
+        // small ? "h-[26rem]" : "h-[40.3125rem]",
+      )}
+    >
+      <div className={clsx("animate-pulse", vertical && "flex")}>
         <div
           className={clsx(
-            "bg-dsfr-background-contrast-green-menthe-active opacity-30",
-            "w-4/5 mx-auto h-3 rounded-xl mt-8",
+            "bg-dsfr-contrast-grey h-52",
+            vertical ? "w-72 rounded-tl-xl rounded-bl-xl" : "w-full rounded-t-xl shrink-0",
           )}
         ></div>
-        <div
-          className={clsx(
-            "bg-dsfr-background-contrast-green-menthe-active opacity-30",
-            "w-4/5 mx-auto h-3 rounded-xl mt-4",
-          )}
-        ></div>
-        <div
-          className={clsx(
-            "bg-dsfr-background-contrast-green-menthe-active opacity-30",
-            "w-4/5 mx-auto h-3 rounded-xl mt-4",
-          )}
-        ></div>
-        <div
-          className={clsx(
-            "bg-dsfr-background-contrast-green-menthe-active opacity-30",
-            "w-4/5 mx-auto h-3 rounded-xl mt-4",
-          )}
-        ></div>
-        <div
-          className={clsx(
-            "bg-dsfr-background-contrast-green-menthe-active opacity-30",
-            "w-4/5 mx-auto h-3 rounded-xl mt-4",
-          )}
-        ></div>
+        <div className={clsx(vertical && "w-full")}>
+          <div
+            className={clsx(
+              "bg-dsfr-background-contrast-green-menthe-active opacity-30",
+              "w-4/5 mx-auto h-3 rounded-xl mt-8",
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "bg-dsfr-background-contrast-green-menthe-active opacity-30",
+              "w-4/5 mx-auto h-3 rounded-xl mt-4",
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "bg-dsfr-background-contrast-green-menthe-active opacity-30",
+              "w-4/5 mx-auto h-3 rounded-xl mt-4",
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "bg-dsfr-background-contrast-green-menthe-active opacity-30",
+              "w-4/5 mx-auto h-3 rounded-xl mt-4",
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "bg-dsfr-background-contrast-green-menthe-active opacity-30",
+              "w-4/5 mx-auto h-3 rounded-xl mt-4",
+            )}
+          ></div>
+        </div>
       </div>
     </div>
   );
