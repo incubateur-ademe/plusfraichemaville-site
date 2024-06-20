a4cc6ea6c675d98ae453566870e857e1d9729b91
mehdilouraoui <med.louraoui@gmail.com>
2024-06-20 17:47:37 +0200

    feat: add financement and soutien ingenierie card

--- components/common/spinner.tsx
+++ components/common/spinner.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 
-/* eslint-disable max-len */
 export const Spinner = ({ className }: { className?: string }) => {
   return (
     <div className={clsx("size-8 content-center rounded-full", className)}>
@@ -9,6 +8,7 @@ export const Spinner = ({ className }: { className?: string }) => {
         <path
           className="opacity-75"
           fill="#000091"
+          /* eslint-disable max-len */
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
