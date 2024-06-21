6d587821a4192baca1a8bd3695aa9c85ed44487f
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-06-21 16:55:56 +0200

    feat: secure aides search endpoint

--- app/api/search-aides-for-estimation/route.ts
+++ app/api/search-aides-for-estimation/route.ts
@@ -4,26 +4,42 @@ import { getFicheSolutionByIds } from "@/lib/strapi/queries/fichesSolutionsQueri
 import { getEstimationById } from "@/lib/prisma/prismaEstimationQueries";
 import { getProjetById } from "@/lib/prisma/prismaProjetQueries";
 import { getCollectiviteById } from "@/lib/prisma/prismaCollectiviteQueries";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/next-auth/auth";
+import { hasPermissionToViewProjet } from "@/actions/projets/permissions";
 
 export async function GET(request: NextRequest) {
-  // TODO : Sécuriser cette route
   const estimationId = request.nextUrl.searchParams.get("estimationId");
-  if (estimationId) {
-    const estimation = await getEstimationById(+estimationId);
-    if (estimation) {
-      const ficheSolutions = await getFicheSolutionByIds(estimation.fiches_solutions_id);
-      const projet = await getProjetById(estimation.projet_id);
-      if (projet) {
-        const collectivite = await getCollectiviteById(projet.collectiviteId);
-        if (collectivite) {
-          const result = await fetchAidesFromAidesTerritoires(
-            ficheSolutions.map((fs) => fs.attributes),
-            collectivite,
-          );
-          return NextResponse.json(result);
-        }
-      }
-    }
+  if (!estimationId) {
+    return NextResponse.json(null, { status: 400 });
+  }
+
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(null, { status: 401 });
+  }
+
+  const estimation = await getEstimationById(+estimationId);
+  if (!estimation) {
+    return NextResponse.json("Estimation not found", { status: 422 });
+  }
+
+  const projet = await getProjetById(estimation.projet_id);
+  if (!projet) {
+    return NextResponse.json("Projet not found", { status: 422 });
+  }
+  if (!(await hasPermissionToViewProjet(projet.id, session.user.id))) {
+    return NextResponse.json(null, { status: 403 });
+  }
+
+  const ficheSolutions = await getFicheSolutionByIds(estimation.fiches_solutions_id);
+  const collectivite = await getCollectiviteById(projet.collectiviteId);
+  if (collectivite) {
+    const result = await fetchAidesFromAidesTerritoires(
+      ficheSolutions.map((fs) => fs.attributes),
+      collectivite,
+    );
+    return NextResponse.json(result);
   }
   return NextResponse.json([]);
 }
