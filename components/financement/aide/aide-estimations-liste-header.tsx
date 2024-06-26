df22bd0ad7eaa977e382326414a6585dbe778a56
mehdilouraoui <med.louraoui@gmail.com>
2024-06-26 13:00:41 +0200

    feat: new card header

--- components/financement/aide/aide-estimations-liste-header.tsx
+++ components/financement/aide/aide-estimations-liste-header.tsx
@@ -6,11 +6,11 @@ export const AideEstimationsListeHeader = ({ projetId, title }: { projetId?: num
     return null;
   }
   return (
-    <div className="mb-10 flex items-center justify-between">
+    <div className="mb-10 flex flex-col items-start justify-between gap-4 lg:flex-row lg:items-center">
       <h1 className="mb-0 max-w-3xl text-[28px] font-bold leading-9">{title}</h1>
       <Link
         href={PFMV_ROUTES.TABLEAU_DE_BORD_WITH_CURRENT_TAB(projetId, "tableau-de-suivi")}
-        className="fr-btn fr-btn--secondary rounded-[20px] !text-sm"
+        className="fr-btn fr-btn--secondary shrink-0 rounded-[20px] !text-sm"
       >
         Retour au tableau de bord
       </Link>
