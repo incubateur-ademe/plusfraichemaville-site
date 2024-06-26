e997e6c199ebae7667f9603b9a001101951b62ab
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-06-26 13:33:29 +0200

    refacto : mutualize type espace label

--- components/tableau-de-bord/tableau-de-bord-suivi-card-info-projet.tsx
+++ components/tableau-de-bord/tableau-de-bord-suivi-card-info-projet.tsx
@@ -2,13 +2,14 @@
 
 import { useProjetsStore } from "@/stores/projets/provider";
 import { DisplayUserName } from "../common/display-user-name";
+import { selectEspaceByCode } from "@/components/filters/TypeEspaceFilter";
 
 export const TableauDeBordSuiviCardInfoProjet = () => {
   const current = useProjetsStore((state) => state.getCurrentProjet());
 
   return (
     <span className="text-sm">
-      <strong>Espace :</strong> <span className="capitalize">{current?.type_espace}</span>
+      <strong>Espace :</strong> <span className="capitalize">{selectEspaceByCode(current?.type_espace)}</span>
       <br />
       <strong>Référent :</strong>
       <DisplayUserName user={current?.creator} />
