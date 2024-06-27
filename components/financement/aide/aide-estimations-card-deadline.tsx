763dc0c5ddf8dba6f577c7efccb0a9b03568d0d3
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-06-27 17:01:10 +0200

    feat: change submission deadline type in db

--- components/financement/aide/aide-estimations-card-deadline.tsx
+++ components/financement/aide/aide-estimations-card-deadline.tsx
@@ -1,6 +1,7 @@
 import { EstimationWithAides } from "@/lib/prisma/prismaCustomTypes";
 import { getAideSubmissionDeadlineAndName } from "../helpers";
 import { AideEstimationsCardWarningRemainingDays } from "./aide-estimations-card-warning-remaining-day";
+import { dateToStringWithoutTime } from "@/helpers/dateUtils";
 
 type AideEstimationsCardDeadlineProps = {
   estimationsAides: EstimationWithAides["estimations_aides"];
@@ -17,7 +18,7 @@ export const AideEstimationsCardDeadline = ({ estimationsAides }: AideEstimation
             <li className="list-none text-lg" key={index}>
               <span className="mr-2 inline-block font-bold">{aideDeadline.name} : </span>
               <span className="text-pretty">
-                Échéance : {aideDeadline.submission_deadline}
+                Échéance : {dateToStringWithoutTime(aideDeadline.submission_deadline)}
                 <AideEstimationsCardWarningRemainingDays submissionDeadline={aideDeadline.submission_deadline} />
               </span>
             </li>
