1f176c38a7c9a18586d210b45e22a53adebcef03
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-07-02 09:29:32 +0200

    fix: fix display aide estimation panel condition

--- components/financement/index.tsx
+++ components/financement/index.tsx
@@ -5,17 +5,16 @@ import { useProjetsStore } from "@/stores/projets/provider";
 
 export const Financement = () => {
   const projet = useProjetsStore((state) => state.getCurrentProjet());
-  const { estimations, fiches_solutions_id: fichesSolutions } = projet || {};
+  const { estimations } = projet || {};
 
   const hasEstimations = estimations && estimations?.length > 0;
-  const hasFichesSolutions = fichesSolutions && fichesSolutions?.length > 0;
   const hasAlreadySelectedAides = !!estimations?.find((estimation) => estimation.estimations_aides.length > 0);
   return (
     <div className="fr-container pt-8">
-      {hasFichesSolutions && hasEstimations && !hasAlreadySelectedAides ? (
-        <AideEstimationListeEmpty />
-      ) : (
+      {hasEstimations || hasAlreadySelectedAides ? (
         <AideEstimationsListe estimations={estimations || []} />
+      ) : (
+        <AideEstimationListeEmpty />
       )}
     </div>
   );
