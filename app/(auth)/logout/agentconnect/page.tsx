1d2816c0f83b40162acc9301ca3c0756112df2e8
mehdilouraoui <med.louraoui@gmail.com>
2024-07-04 16:44:29 +0200

    fix: build

--- app/(auth)/logout/agentconnect/page.tsx
+++ app/(auth)/logout/agentconnect/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import { PFMV_ROUTES } from "@/helpers/routes";
 import { Page } from "@/types/global";
 
-export const Logout = async (props: Page) => {
+const Logout = async (props: Page) => {
   const logOutUrl = new URL(`${process.env.AGENT_CONNECT_BASE_URL}/v2/session/end`);
   logOutUrl.searchParams.set("id_token_hint", props.searchParams.id_token_hint || "");
   logOutUrl.searchParams.set("post_logout_redirect_uri", process.env.NEXTAUTH_URL + PFMV_ROUTES.DECONNEXION);
