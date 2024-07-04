fce777b2506998cbdd3186ea6766362300c37dbb
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-07-04 09:09:32 +0200

    refacto: change update projet state method

--- stores/projets/store.ts
+++ stores/projets/store.ts
@@ -1,8 +1,6 @@
 import { EstimationAide, ProjetWithRelations } from "@/lib/prisma/prismaCustomTypes";
 import { createStore } from "zustand/vanilla";
 import { upsert } from "@/helpers/listUtils";
-import { updateFichesProjetAction } from "@/actions/projets/update-fiches-projet-action";
-import { notifications } from "@/components/common/notifications";
 import { updateAideInEstimation } from "./helper";
 
 export interface ProjetsState {
@@ -18,12 +16,6 @@ export type ProjetsActions = {
   addOrUpdateProjet: (_projet: ProjetWithRelations) => void;
   addAideInEstimation: (_estimationId: number, _estimationAide: EstimationAide) => void;
   deleteAideInEstimation: (_estimationId: number, _aideTerritoireId: number) => void;
-  updateSelectedFiches: (
-    _type: "solution" | "diagnostic",
-    _ficheId: number,
-    _projetId: number,
-    _withNotification?: boolean,
-  ) => void;
   deleteProjet: (_projetId: number) => void;
 };
 
@@ -49,15 +41,6 @@ export const createProjetStore = (initState: ProjetsState = defaultInitState) =>
     },
     getProjetById: (projetId) => get().projets.find((projet) => projet.id === projetId),
     addOrUpdateProjet: (_projet) => set((state) => ({ projets: upsert(state.projets, _projet) })),
-    updateSelectedFiches: async (type, ficheId, projetId, withNotification) => {
-      const update = await updateFichesProjetAction(projetId, ficheId, type);
-      if (update.projet) {
-        set((state) => ({ projets: upsert(state.projets, update.projet!) }));
-        if (withNotification) {
-          notifications(update.type, update.message);
-        }
-      }
-    },
     deleteProjet: async (projetId) => {
       set((state) => ({
         projets: state.projets.filter((projet) => projet.id !== projetId),
