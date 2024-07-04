7633dfc2f356ebcdf5e1ec7f825aa37538795d2b
mehdilouraoui <med.louraoui@gmail.com>
2024-07-04 17:14:25 +0200

    fix: change callback url for deconnexion

--- app/(auth)/logout/page.tsx
+++ app/(auth)/logout/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
-import { redirect, useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Logout() {
   const router = useRouter();
   const session = useSession();
-  const isLogout = useSearchParams().get("state") || session.status === "unauthenticated";
   useEffect(() => {
     if (session.data) {
       const callbackUrl = `/logout/agentconnect?id_token_hint=${session.data?.id_token}`;
       signOut({ redirect: false, callbackUrl }).then((signOutResponse) => router.push(signOutResponse.url));
-    } else if (isLogout) {
-      redirect("/");
     }
-  }, [isLogout, router, session]);
+  }, [router, session]);
 
   return <div />;
 }
