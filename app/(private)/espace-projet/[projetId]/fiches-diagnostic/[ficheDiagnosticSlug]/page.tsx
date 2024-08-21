b10ec57f41f61af4389a1a85cb39a9c9f0d72250
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-08-21 16:10:07 +0200

    fix: fiche solution and fiche diagnostic pages should be available for user with role lecteur

--- app/(private)/espace-projet/[projetId]/fiches-diagnostic/[ficheDiagnosticSlug]/page.tsx
+++ app/(private)/espace-projet/[projetId]/fiches-diagnostic/[ficheDiagnosticSlug]/page.tsx
@@ -1,4 +1,3 @@
-import { ProtectedEspaceProjetUrl } from "@/components/common/protected-espace-projet-url";
 import { FicheDiagnostic } from "@/components/fiches-diagnostic/fiche-diagnostic";
 import { getFicheDiagnosticBySlug } from "@/lib/strapi/queries/fiches-diagnostic-queries";
 import { notFound } from "next/navigation";
@@ -9,9 +8,5 @@ export default async function FicheDiagnosticPage({ params }: { params: { ficheD
   if (!ficheDiagnostic) {
     return notFound();
   }
-  return (
-    <ProtectedEspaceProjetUrl>
-      <FicheDiagnostic ficheDiagnostic={ficheDiagnostic} />
-    </ProtectedEspaceProjetUrl>
-  );
+  return <FicheDiagnostic ficheDiagnostic={ficheDiagnostic} />;
 }
--- app/(private)/espace-projet/[projetId]/fiches-solutions/liste/[ficheSolutionSlug]/page.tsx
+++ app/(private)/espace-projet/[projetId]/fiches-solutions/liste/[ficheSolutionSlug]/page.tsx
@@ -1,4 +1,3 @@
-import { ProtectedEspaceProjetUrl } from "@/components/common/protected-espace-projet-url";
 import { FicheSolution } from "@/components/ficheSolution/fiche-solution";
 
 export default async function FicheSolutionPage({
@@ -8,9 +7,5 @@ export default async function FicheSolutionPage({
   params: { ficheSolutionSlug: string; projetId: string };
   searchParams: { etapeAideDecision: string | undefined };
 }) {
-  return (
-    <ProtectedEspaceProjetUrl>
-      <FicheSolution params={params} searchParams={searchParams} />
-    </ProtectedEspaceProjetUrl>
-  );
+  return <FicheSolution params={params} searchParams={searchParams} />;
 }
