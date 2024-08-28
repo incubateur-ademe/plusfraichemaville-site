8ea0ec3ab200d4080d1c0c808287630f8d853d32
mehdilouraoui <med.louraoui@gmail.com>
2024-08-28 17:28:50 +0200

    feat: save conversation id in local storage

--- components/agent-conversationnel/hooks/use-ai-chat.ts
+++ components/agent-conversationnel/hooks/use-ai-chat.ts
@@ -2,16 +2,18 @@ import { saveConversationAction } from "@/actions/agent-conversationnel/save-con
 import { sentChatMessageAction } from "@/actions/agent-conversationnel/send-chat-message-action";
 import { useAiChatApi, useAsBatchAdapter } from "@nlux/react";
 import { useState } from "react";
+import { useLocalStorage } from "usehooks-ts";
 
 export const useAiChat = () => {
   const [conversationId, setConversationId] = useState<string | null>(null);
-
+  const [, saveConversationIdInLocalStorage] = useLocalStorage<string>("conversation-id", "");
   const aiApi = useAiChatApi();
 
   const chatAdapter = useAsBatchAdapter(async (message: string): Promise<string> => {
     const result = await sentChatMessageAction(message, conversationId);
     if (!conversationId) {
       saveConversationAction(result.conversationId);
+      saveConversationIdInLocalStorage(result.conversationId);
       setConversationId(result.conversationId);
     }
 
