310486467ec0f3eee1e3fb726a34403d78aa9747
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-09-26 11:55:40 +0200

    fix: try to fix build

--- src/services/hubspot/index.ts
+++ src/services/hubspot/index.ts
@@ -1,7 +1,7 @@
-import { Client } from "@hubspot/api-client";
+import * as hubspot from "@hubspot/api-client";
 import { ContactFormData } from "@/src/forms/contact/contact-form-schema";
 
-const hubspotClient = new Client({ accessToken: process.env.HUBSPOT_ACCESS_TOKEN });
+const hubspotClient = new hubspot.Client({ accessToken: process.env.HUBSPOT_ACCESS_TOKEN });
 
 export const createHubspotTicket = async (data: ContactFormData) => {
   const properties = {
