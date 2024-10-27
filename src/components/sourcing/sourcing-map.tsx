124d1df6629449f2abda30bc8cdd3073600ecd69
mehdilouraoui <med.louraoui@gmail.com>
2024-10-27 21:23:54 +0100

    fix: remove unecessary files

--- src/components/sourcing/sourcing-map.tsx
+++ src/components/sourcing/sourcing-map.tsx
@@ -1,8 +1,5 @@
 import { getPublicProjets } from "@/src/lib/prisma/prismaProjetQueries";
-import {
-  getRetoursExperiences,
-  getRetoursExperiencesWithSourcing,
-} from "@/src/lib/strapi/queries/retoursExperienceQueries";
+import { getRetoursExperiencesWithSourcing } from "@/src/lib/strapi/queries/retoursExperienceQueries";
 
 export const SourcingMap = async () => {
   const inProgressProjets = await getPublicProjets();
