7e8e7f481aee503357e8c357c35aab718e010ed2
mehdilouraoui <med.louraoui@gmail.com>
2024-10-28 19:53:25 +0100

    feat: modify zoom

--- src/components/sourcing/sourcing-map-client.tsx
+++ src/components/sourcing/sourcing-map-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap, ZoomControl } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { LatLngTuple } from "leaflet";
 import { useProjetCoordinates } from "./hooks";
@@ -17,7 +17,7 @@ export type SourcingMapClientProps = {
 export const SourcingMapClient = ({ markers }: SourcingMapClientProps) => {
   const coordinates = useProjetCoordinates();
   return (
-    <MapContainer className="h-[715px]" center={[48.8566, 2.3522]} zoom={5}>
+    <MapContainer className="h-[715px]" center={[48.8566, 2.3522]} zoom={5} zoomControl={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -28,6 +28,7 @@ export const SourcingMapClient = ({ markers }: SourcingMapClientProps) => {
         ))}
       </MarkerClusterGroup>
       {coordinates && <Marker position={coordinates} icon={createCustomIcon("collectivite")} />}
+      <ZoomControl position="topleft" />
     </MapContainer>
   );
 };
