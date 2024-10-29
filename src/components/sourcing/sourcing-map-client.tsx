d7cd27ce5024bfdcbe7dd2f08fefd364835806fe
mehdilouraoui <med.louraoui@gmail.com>
2024-10-29 10:38:05 +0100

    fix: lint

--- src/components/sourcing/sourcing-map-client.tsx
+++ src/components/sourcing/sourcing-map-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { LatLngTuple } from "leaflet";
 import { useProjetCoordinates } from "./hooks";
