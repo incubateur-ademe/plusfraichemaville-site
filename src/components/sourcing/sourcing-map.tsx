b6093c72ca67d5d0c2d9b3664b82a2accfe1f3db
mehdilouraoui <med.louraoui@gmail.com>
2024-10-29 10:37:16 +0100

    fix: lint

--- src/components/sourcing/sourcing-map.tsx
+++ src/components/sourcing/sourcing-map.tsx
@@ -1,9 +1,3 @@
-import { getPublicProjets } from "@/src/lib/prisma/prismaProjetQueries";
-import { getRetoursExperiencesWithSourcing } from "@/src/lib/strapi/queries/retoursExperienceQueries";
-
 export const SourcingMap = async () => {
-  const inProgressProjets = await getPublicProjets();
-  const rexProjets = await getRetoursExperiencesWithSourcing();
-
   return <div></div>;
 };
