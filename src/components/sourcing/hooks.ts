42c91d383eeb20e8a09d0264fdc2ab224f749faf
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-10-31 14:45:30 +0100

    feat: should display current projet collectivite as red pin

--- src/components/sourcing/hooks.ts
+++ src/components/sourcing/hooks.ts
@@ -2,20 +2,15 @@
 
 import { useProjetsStore } from "@/src/stores/projets/provider";
 import { LatLngTuple } from "leaflet";
-import { GeoJsonAdresse } from "./types";
-import { lambert93toWGPS } from "@/src/helpers/convert-coordinates";
 
 export const useCurrentProjetCoordinates = () => {
   const projet = useProjetsStore((state) => state.getCurrentProjet());
   if (!projet) {
     return null;
   }
-  const adresseInfo = projet?.adresse_info as unknown as GeoJsonAdresse["properties"];
   const collectivite = projet?.collectivite;
 
-  const coordinates = adresseInfo
-    ? lambert93toWGPS(adresseInfo.x, adresseInfo.y)
-    : { latitude: collectivite?.latitude, longitude: collectivite?.longitude };
+  const coordinates = { latitude: collectivite?.latitude, longitude: collectivite?.longitude };
 
   return [coordinates.latitude, coordinates.longitude] as LatLngTuple;
 };
