c0f678c7edbfcfb03546d237aabdbe610a445d87
mehdilouraoui <med.louraoui@gmail.com>
2024-11-04 15:30:27 +0100

    feat: remove coverage hover

--- src/components/sourcing/sourcing-map-client.tsx
+++ src/components/sourcing/sourcing-map-client.tsx
@@ -31,7 +31,7 @@ const SourcingMapClient = ({ markers }: SourcingMapClientProps) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
+      <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon} showCoverageOnHover={false}>
         {markers.map((marker, index) => (
           <Marker position={marker.geocode} key={index} icon={createCustomIcon(marker.type)} />
         ))}
