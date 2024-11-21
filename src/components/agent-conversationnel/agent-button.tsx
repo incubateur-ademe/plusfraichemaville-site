7c08f3ed7cd22beeb70a57f299350813b8477997
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-11-21 16:16:26 +0100

    feat: add trackevent when opening zephyr

--- src/components/agent-conversationnel/agent-button.tsx
+++ src/components/agent-conversationnel/agent-button.tsx
@@ -1,14 +1,20 @@
 import clsx from "clsx";
 import Image from "next/image";
+import { trackEvent } from "@/src/helpers/matomo/track-matomo";
+import { OPEN_ZEPHYR } from "@/src/helpers/matomo/matomo-tags";
 
 type AgentButtonProps = {
   openChat: () => void;
 };
 
 export const AgentButton = ({ openChat }: AgentButtonProps) => {
+  const onClick = () => {
+    trackEvent(OPEN_ZEPHYR);
+    openChat();
+  };
   return (
     <button
-      onClick={openChat}
+      onClick={onClick}
       className={clsx(
         "fixed bottom-10 right-10 flex items-center gap-[10px] rounded-2xl bg-white px-3 py-[10px]",
         " z-[900] shadow-[0px_6px_18px_0px_rgba(0,0,18,0.16)]",
--- src/helpers/matomo/matomo-tags.ts
+++ src/helpers/matomo/matomo-tags.ts
@@ -12,6 +12,7 @@ export const MATOMO_CATEGORIES = {
 export const MATOMO_ACTIONS = {
   SUBSCRIBE_WEBINAIRE: "webinaire-inscription",
   UPDATE_MATURITE: "update-maturite",
+  OPEN_AGENT_BUTTON: "open-agent-button",
 };
 
 export const WEBINAIRE_SUBSCRIPTION: MATOMO_EVENT = {
@@ -24,4 +25,10 @@ export const UPDATE_MATURITE: MATOMO_EVENT = {
   category: MATOMO_CATEGORIES.ESPACE_PROJET,
   action: MATOMO_ACTIONS.UPDATE_MATURITE,
   name: "Changment de maturité via l'icone",
+};
+
+export const OPEN_ZEPHYR: MATOMO_EVENT = {
+  category: MATOMO_CATEGORIES.SITE_PUBLIC,
+  action: MATOMO_ACTIONS.OPEN_AGENT_BUTTON,
+  name: "Ouverture de l'Agent à partir du bouton",
 };
