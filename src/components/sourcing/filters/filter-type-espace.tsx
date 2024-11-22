b94bc60941ec02189ec7d71d4f2abb6bb2ae7213
mehdilouraoui <med.louraoui@gmail.com>
2024-11-22 15:02:43 +0100

    fix accordion type espace

--- src/components/sourcing/filters/filter-type-espace.tsx
+++ src/components/sourcing/filters/filter-type-espace.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
+
 import { ALL_ESPACES, TypeEspaceCode } from "../../filters/TypeEspaceFilter";
 import { SourcingFiltersAccordion } from "./filters-accordion";
-import clsx from "clsx";
+import Checkbox from "@codegouvfr/react-dsfr/Checkbox";
 type SourcingFilterTypeEspaceProps = {
   selectedTypeEspace: TypeEspaceCode[];
   setSelectedTypeEspace: (_type: TypeEspaceCode[]) => void;
@@ -20,25 +20,17 @@ export const SourcingFilterTypeEspace = ({
   };
   return (
     <SourcingFiltersAccordion label="Type d'espaces">
-      <div className="flex flex-col gap-3">
-        {ALL_ESPACES.map((espace) => (
-          <div
-            onClick={() => handleSelectedType(espace.code)}
-            className={clsx(
-              "flex items-center gap-2 rounded-2xl px-[18px] py-4 text-sm",
-              "cursor-pointer shadow-pfmv-navy-shadow",
-              "border-[1px]",
-              selectedTypeEspace?.includes(espace.code)
-                ? "bg-pfmv-navy/10 border-pfmv-navy"
-                : "border-white hover:border-pfmv-navy",
-            )}
-            key={espace.code}
-          >
-            <Image width={55} height={55} src={`/images/espaces/${espace.icon}`} alt={espace.label} />
-            {espace.label}
-          </div>
-        ))}
-      </div>
+      <Checkbox
+        className="h-72"
+        options={ALL_ESPACES.map((espace) => ({
+          label: espace.label,
+          value: espace.code,
+          nativeInputProps: {
+            onChange: () => handleSelectedType(espace.code),
+            checked: selectedTypeEspace?.includes(espace.code),
+          },
+        }))}
+      />
     </SourcingFiltersAccordion>
   );
 };
