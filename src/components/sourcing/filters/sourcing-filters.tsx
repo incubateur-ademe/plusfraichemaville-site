20594195fb001f34a931a62a001ffa81efb549c7
mehdilouraoui <med.louraoui@gmail.com>
2024-11-26 19:28:09 +0100

    fix height regression

--- src/components/sourcing/filters/sourcing-filters.tsx
+++ src/components/sourcing/filters/sourcing-filters.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export const SourcingFilters = ({ children }: PropsWithChildren) => {
   return (
-    <div className="relative min-h-20">
+    <div className="relative h-[12.5rem] md:h-[9.5rem] xl:h-20">
       <div className="mb-6 flex w-fit flex-wrap gap-4">{children}</div>
     </div>
   );
