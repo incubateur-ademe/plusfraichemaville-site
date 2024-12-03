69d8e9041ce2f66272aa8376edcdb80a27931265
mehdilouraoui <med.louraoui@gmail.com>
2024-12-03 14:41:08 +0100

    feat: add controller timeout for long request

--- src/services/mattermost/index.ts
+++ src/services/mattermost/index.ts
@@ -1,13 +1,28 @@
+import { customCaptureException } from "@/src/lib/sentry/sentryCustomMessage";
+
 const webhooks = {
   hubspot: process.env.MATTERMOST_WEBHOOK_HUBSPOT_URL ?? "",
 } as const;
 
-export const sendMattermostWebhook = async <T>(data: T, webhook: keyof typeof webhooks) => {
-  await fetch(webhooks[webhook], {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+const WEBHOOK_TIMEOUT_DURATION = 3000;
+
+export const sendMattermostWebhook = async <T>(data: T, webhookKey: keyof typeof webhooks) => {
+  const webhook = webhooks[webhookKey];
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), WEBHOOK_TIMEOUT_DURATION);
+
+  try {
+    await fetch(webhook, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      signal: controller.signal,
+    });
+  } catch (error) {
+    customCaptureException(`Error in following Mattermost webhook : ${webhook}`, error);
+  } finally {
+    clearTimeout(timeout);
+  }
 };
