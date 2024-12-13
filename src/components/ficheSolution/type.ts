2ee7e89b9bbfe23181b1b9f56db08932862a9415
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2024-12-13 16:10:38 +0100

    chore: fix merge

--- src/components/ficheSolution/type.ts
+++
@@ -1,7 +0,0 @@
-// import { APIResponseData, GetValues } from "@/src/lib/strapi/types/types";
-//
-// export type FicheSolution = GetValues<"api::fiche-solution.fiche-solution">;
-// export type FicheSolutionResponse = APIResponseData<"api::fiche-solution.fiche-solution">;
-// export type RetourExperienceResponse = APIResponseData<"api::retour-experience.retour-experience">;
-// export type MateriauResponse = APIResponseData<"api::materiau.materiau">;
-// export type Materiau = GetValues<"api::materiau.materiau">;
