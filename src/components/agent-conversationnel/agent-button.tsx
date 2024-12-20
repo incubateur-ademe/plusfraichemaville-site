82241c38b021c29e52d273293c25353e74830933
mehdilouraoui <med.louraoui@gmail.com>
2024-12-20 12:11:18 +0100

    fix following comments

--- src/components/agent-conversationnel/agent-button.tsx
+++ src/components/agent-conversationnel/agent-button.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { trackEvent } from "@/src/helpers/matomo/track-matomo";
 import { OPEN_ZEPHYR } from "@/src/helpers/matomo/matomo-tags";
-import { AgentButtonPicto } from "./agent-button-picto";
+import Image from "next/image";
 
 type AgentButtonProps = {
   controllers: {
@@ -20,15 +20,14 @@ export const AgentButton = ({ controllers }: AgentButtonProps) => {
       disabled={controllers.isOpen}
       onClick={onClick}
       className={clsx(
-        "fixed right-0 top-80 flex h-14 w-32 items-center gap-3 !bg-pfmv-navy px-3",
+        "fixed right-0 top-36 flex h-14 w-32 items-center gap-3 !bg-pfmv-navy px-3 md:top-80",
         "rounded-l-2xl",
-        "transition-transform duration-300",
+        "z-[900] transition-transform duration-300",
         "translate-x-[84px] hover:translate-x-0",
-        "z-[900] shadow-[0px_6px_18px_0px_rgba(0,0,18,0.16)]",
         "disabled:pointer-events-none disabled:hidden",
       )}
     >
-      <AgentButtonPicto />
+      <Image src="/images/zephyr/question.svg" alt="Agent" width={24} height={24} className="size-6" />
       <span className="text-sm leading-4 text-white">Posez vos questions</span>
     </button>
   );
