1e132ada4bf4b449142ff09c7d4a2aa2ac71b3a3
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-01-06 11:16:53 +0100

    feat: use old prisma raw query way as new one does not work on CI CD.

--- src/lib/prisma/prisma-analytics-queries.ts
+++ src/lib/prisma/prisma-analytics-queries.ts
@@ -1,6 +1,7 @@
-import { Analytics } from "@prisma/client";
+/* eslint-disable max-len */
+
+import { Analytics, Prisma } from "@prisma/client";
 import { prismaClient } from "./prismaClient";
-import { northStarStatQuery } from "@prisma/client/sql";
 import { DateRange } from "@/src/helpers/dateUtils";
 
 type AnalyticsProps = Omit<Analytics, "id" | "created_at" | "created_by">;
@@ -27,5 +28,23 @@ type NorthStarQueryRecord = {
 };
 
 export const getNorthStarStats = async (params: GetNorthStarStatsProps): Promise<NorthStarQueryRecord[]> => {
-  return prismaClient.$queryRawTyped(northStarStatQuery(params.dateFrom, params.range));
+  return prismaClient.$queryRaw(
+    Prisma.sql`with score_table as (select date_trunc(${params.range}, created_at)                                    as date1,
+                                           sum(CASE WHEN event_type = 'UPDATE_PROJET_SET_VISIBLE' THEN 1 ELSE -1 END) as score
+                                    from pfmv."Analytics"
+                                    WHERE event_type in
+                                          ('UPDATE_PROJET_SET_VISIBLE',
+                                           'UPDATE_PROJET_SET_INVISIBLE')
+                                      and created_at >= ${params.dateFrom}
+                                    group by 1
+                                    order by 1 desc),
+                    all_intervals as (SELECT date_trunc(${params.range}, ts) as date1
+                                      FROM generate_series(${params.dateFrom}, now(), CONCAT('1 ', ${params.range})::interval) AS ts
+                                      group by 1)
+               select all_intervals.date1::timestamp::date as "periode",
+                      coalesce(score, 0)                   as "score"
+               from score_table
+                        right outer join all_intervals on all_intervals.date1 = score_table.date1
+               order by 1;`,
+  );
 };
