138d65036dc89edc6961352ec8bffa2a133c8dfc
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-01-13 14:17:41 +0100

    chore: upgrade sentry sdk

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,7 +8,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://8d4940c318ab411f9ce8eb5d095f5373@sentry.incubateur.net/120",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 0.2,
   environment: process.env.NEXT_PUBLIC_SENTRY_ENV,
 
