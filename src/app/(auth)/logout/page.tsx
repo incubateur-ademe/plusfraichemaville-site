747cf72f361e2741103129f24d4c4959fd76220d
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-01-13 18:27:45 +0100

    fix: remove hubspot cookie when logging out

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -2,12 +2,14 @@
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { removeHubspotCookies } from "@/src/components/hubspot/track-hubspot";
 
 export default function Logout() {
   const router = useRouter();
   const session = useSession();
   useEffect(() => {
     if (session.data) {
+      removeHubspotCookies();
       const callbackUrl = `/logout/agentconnect?id_token_hint=${session.data?.id_token}`;
       signOut({ redirect: false, callbackUrl }).then((signOutResponse) => router.push(signOutResponse.url));
     }
