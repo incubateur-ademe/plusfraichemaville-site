62977523a5417020868efa3c734439c1735cc32a
mehdilouraoui <med.louraoui@gmail.com>
2025-02-17 16:09:53 +0100

    fix: async params

--- src/app/(private)/espace-projet/[projetId]/fiches-diagnostic/prestation/liste/page.tsx
+++ src/app/(private)/espace-projet/[projetId]/fiches-diagnostic/prestation/liste/page.tsx
@@ -1,7 +1,8 @@
 import { FicheDiagnosticChoix } from "@/src/components/fiches-diagnostic/fiche-diagnostic-choix";
 import { ProtectedEspaceProjetUrl } from "@/src/components/common/protected-espace-projet-url";
 
-export default async function FicheDiagnosticListePage({ params }: { params: { projetId: number } }) {
+export default async function FicheDiagnosticListePage(props: { params: Promise<{ projetId: number }> }) {
+  const params = await props.params;
   return (
     <ProtectedEspaceProjetUrl>
       <div className="fr-container--fluid pt-8 text-black">
