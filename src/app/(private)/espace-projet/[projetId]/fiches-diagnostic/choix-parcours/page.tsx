e98650f4f6b68ef680b28b54fe5ab713cb371020
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-03-11 10:20:21 +0100

    feat: page choix parcours du module diag

--- src/app/(private)/espace-projet/[projetId]/fiches-diagnostic/choix-parcours/page.tsx
+++ src/app/(private)/espace-projet/[projetId]/fiches-diagnostic/choix-parcours/page.tsx
@@ -4,7 +4,12 @@ export default async function FicheDiagnosticChoixParcoursPage() {
   return (
     <div className="fr-container--fluid pt-8 text-black">
       <div className="fr-container">
-        <h1 className="mb-6 text-2xl font-bold">Je fais un diagnostic de la surchauffe sur l’espace à rafraîchir</h1>
+        <h1 className="mb-6 text-2xl font-bold">Je fais un diagnostic de surchauffe sur l’espace à rafraîchir</h1>
+        <div className={"mb-12 text-lg"}>
+          {"Il est nécessaire d’effectuer un diagnostic que ce soit pour avoir un suivi de l’évolution de" +
+            " la surchauffe ou encore mieux cibler les solutions. Vous pouvez calculer les indicateurs" +
+            " environnementaux en open source ou encore faire appel à un bureau d’étude pour un diagnostic approfondi."}
+        </div>
         <FicheDiagnosticChoixParcours />
       </div>
     </div>
