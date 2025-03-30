2bde25f04e78c0f4ee90aaea33a4d00da569eb01
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-03-30 17:30:50 +0200

    feat: breadcrumb for diagnostic parcours 2

--- src/components/espace-projet/banner/breadcurmb-list.ts
+++ src/components/espace-projet/banner/breadcurmb-list.ts
@@ -21,6 +21,20 @@ const BREADCRUMB_SEGMENT_DIAG_PRESTATION_LISTE = (projetId: number): BreadcrumbS
   label: "Liste des prestations de diagnostic",
 });
 
+const BREADCRUMB_SEGMENT_DIAG_INDICATEURS_PRESENTATION = (projetId: number): BreadcrumbSegment => ({
+  linkProps: {
+    href: PFMV_ROUTES.ESPACE_PROJET_DIAGNOSTIC_INDICATEURS_PRESENTATION(projetId),
+  },
+  label: "Présentation des coefficients",
+});
+
+const BREADCRUMB_SEGMENT_DIAG_INDICATEURS_QUESTIONS = (projetId: number): BreadcrumbSegment => ({
+  linkProps: {
+    href: PFMV_ROUTES.ESPACE_PROJET_DIAGNOSTIC_INDICATEURS_QUESTIONS(projetId),
+  },
+  label: "Saisie des informations",
+});
+
 type BreadcrumbStep = {
   currentPageLabel: string;
   matchSegments: string[];
@@ -40,7 +54,7 @@ export const BREADCRUMB_DIAG_REX = (projetId: number): BreadcrumbStep => ({
 });
 
 export const BREADCRUMB_DIAG_PRESTATION_LISTE = (projetId: number): BreadcrumbStep => ({
-  currentPageLabel: "Liste des prestations de diagnostic",
+  currentPageLabel: BREADCRUMB_SEGMENT_DIAG_PRESTATION_LISTE(projetId).label,
   matchSegments: ["diagnostic", "prestation", "liste"],
   breadcrumbSegments: [BREADCRUMB_SEGMENT_DASHBOARD(projetId)],
 });
@@ -51,9 +65,37 @@ export const BREADCRUMB_DIAG_PRESTATION_SELECTION = (projetId: number): Breadcru
   breadcrumbSegments: [BREADCRUMB_SEGMENT_DASHBOARD(projetId)],
 });
 
+export const BREADCRUMB_DIAG_INDICATEURS_PRESENTATION = (projetId: number): BreadcrumbStep => ({
+  currentPageLabel: BREADCRUMB_SEGMENT_DIAG_INDICATEURS_PRESENTATION(projetId).label,
+  matchSegments: ["diagnostic", "indicateurs-environnementaux", "presentation"],
+  breadcrumbSegments: [BREADCRUMB_SEGMENT_DASHBOARD(projetId)],
+});
+
+export const BREADCRUMB_DIAG_INDICATEURS_QUESTIONS = (projetId: number): BreadcrumbStep => ({
+  currentPageLabel: BREADCRUMB_SEGMENT_DIAG_INDICATEURS_QUESTIONS(projetId).label,
+  matchSegments: ["diagnostic", "indicateurs-environnementaux", "questions"],
+  breadcrumbSegments: [
+    BREADCRUMB_SEGMENT_DASHBOARD(projetId),
+    BREADCRUMB_SEGMENT_DIAG_INDICATEURS_PRESENTATION(projetId),
+  ],
+});
+
+export const BREADCRUMB_DIAG_INDICATEURS_RESULTATS = (projetId: number): BreadcrumbStep => ({
+  currentPageLabel: "Résultats de mon diagnostic",
+  matchSegments: ["diagnostic", "indicateurs-environnementaux", "resultats"],
+  breadcrumbSegments: [
+    BREADCRUMB_SEGMENT_DASHBOARD(projetId),
+    BREADCRUMB_SEGMENT_DIAG_INDICATEURS_PRESENTATION(projetId),
+    BREADCRUMB_SEGMENT_DIAG_INDICATEURS_QUESTIONS(projetId),
+  ],
+});
+
 export const ALL_BREADCRUMB_STEPS = (projetId: number): BreadcrumbStep[] => [
   BREADCRUMB_DIAG_FICHE(projetId),
   BREADCRUMB_DIAG_REX(projetId),
   BREADCRUMB_DIAG_PRESTATION_LISTE(projetId),
   BREADCRUMB_DIAG_PRESTATION_SELECTION(projetId),
+  BREADCRUMB_DIAG_INDICATEURS_PRESENTATION(projetId),
+  BREADCRUMB_DIAG_INDICATEURS_QUESTIONS(projetId),
+  BREADCRUMB_DIAG_INDICATEURS_RESULTATS(projetId),
 ];
