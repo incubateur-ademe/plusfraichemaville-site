c564f34ca38a0f4702bb30ad0ecac6c7a257f0da
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-06-06 16:38:06 +0200

    feat: normalize layout

--- src/app/(site-vitrine)/aide-decision/layout.tsx
+++ src/app/(site-vitrine)/aide-decision/layout.tsx
@@ -4,7 +4,7 @@ import AppFooter from "@/src/components/layout/AppFooter";
 export default function Layout({ children }: { children: ReactElement | null }) {
   return (
     <>
-      <div className={"aide-decision-background pb-40"}>{children}</div>
+      <div className={"aide-decision-background"}>{children}</div>
       <AppFooter />
     </>
   );
