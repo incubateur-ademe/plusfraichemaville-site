dd1a341b6a36a429baa91e4032d33c86a1d65bdc
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-07-03 10:48:55 +0200

    feat: "se connecter" button redirects to pro connect page.

--- src/components/authentication/auth-buttons.tsx
+++ src/components/authentication/auth-buttons.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { AuthButtonEspaceProjet } from "./auth-button-espace-projet";
 import { AuthButtonUser } from "./auth-button-user";
-import { useSession } from "next-auth/react";
-import { PFMV_ROUTES } from "@/src/helpers/routes";
-import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
 import { Case, Conditional, Default } from "@/src/components/common/conditional-renderer";
+import Button from "@codegouvfr/react-dsfr/Button";
+import { getFullUrl, PFMV_ROUTES } from "@/src/helpers/routes";
 
 export const AuthButtons = () => {
   const { status } = useSession();
+
+  const handleSignIn = () => signIn("agentconnect", { callbackUrl: getFullUrl(PFMV_ROUTES.ESPACE_PROJET_LISTE) });
 
   return (
     <div className="hidden items-center gap-4 lg:flex">
@@ -19,13 +21,15 @@ export const AuthButtons = () => {
           <div className="h-10 w-40 animate-pulse rounded-3xl bg-dsfr-background-alt-grey" />
         </Case>
         <Default>
-          <Link
-            className="fr-btn fr-btn--tertiary-no-outline fr-icon-user-line fr-btn--icon-left rounded-3xl"
-            href={PFMV_ROUTES.ESPACE_PROJET}
-            prefetch={false}
+          <Button
+            onClick={() => handleSignIn()}
+            priority="tertiary no outline"
+            iconId="fr-icon-user-line"
+            iconPosition="left"
+            className="rounded-3xl"
           >
             Se connecter
-          </Link>
+          </Button>
         </Default>
       </Conditional>
     </div>
