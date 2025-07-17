ba13a9a55676ae565f411627ab9b70245eb3c1b3
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-07-17 11:17:42 +0200

    feat: home slide keyboard navigation

--- src/components/homepage/homepage-story.tsx
+++ src/components/homepage/homepage-story.tsx
@@ -21,30 +21,29 @@ export const HomepageStory = ({ story }: HomepageStoryProps) => {
 
   return (
     <div className="group">
-      <LinkWithoutPrefetch href={PFMV_ROUTES.FICHE_SOLUTION(story.slug)}>
-        <div
-          className={clsx(
-            "flex shrink-0 flex-col justify-end rounded-2xl pt-5 md:h-[540px] md:w-[358px]",
-            "relative h-[296px] w-[210px] overflow-hidden",
-            "px-5 py-5 md:px-8 md:py-10",
-            `gradient-solution-${typeSolution?.code}`,
-          )}
-        >
-          {typeSolution && (
-            <div className="mb-2 flex text-sm text-white md:text-lg">
-              {typeSolution.icon("fr-icon--sm mr-2 mb-auto")}
-              <span className="mt-[1px]">{typeSolution.label}</span>
-            </div>
-          )}
-          <h3 className="m-0 text-base text-white md:text-[22px] md:leading-7">{story.title}</h3>
-          <Image
-            src={story.image}
-            alt={story.alt}
-            fill
-            sizes="60vw"
-            className="-z-10 object-cover transition-transform duration-300 ease-in group-hover:scale-[1.05]"
-          />
-        </div>
+      <LinkWithoutPrefetch
+        className={clsx(
+          "flex shrink-0 flex-col justify-end rounded-2xl pt-5 md:h-[540px] md:w-[358px]",
+          "relative h-[296px] w-[210px] overflow-hidden",
+          "px-5 py-5 md:px-8 md:py-10",
+          `gradient-solution-${typeSolution?.code}`,
+        )}
+        href={PFMV_ROUTES.FICHE_SOLUTION(story.slug)}
+      >
+        {typeSolution && (
+          <div className="mb-2 flex text-sm text-white md:text-lg">
+            {typeSolution.icon("fr-icon--sm mr-2 mb-auto")}
+            <span className="mt-[1px]">{typeSolution.label}</span>
+          </div>
+        )}
+        <h3 className="m-0 text-base text-white md:text-[22px] md:leading-7">{story.title}</h3>
+        <Image
+          src={story.image}
+          alt={story.alt}
+          fill
+          sizes="60vw"
+          className="-z-10 object-cover transition-transform duration-300 ease-in group-hover:scale-[1.05]"
+        />
       </LinkWithoutPrefetch>
     </div>
   );
