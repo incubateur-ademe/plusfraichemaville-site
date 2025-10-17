38f54dbf31a16b9b409abd30f0ca47ba2d5e479e
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2025-10-17 19:28:16 +0200

    feat: design for new contact form

--- src/app/(static)/contact/page.tsx
+++ src/app/(static)/contact/page.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = computeMetadata("Contactez-nous");
 
 export default async function PageContact() {
   return (
-    <div className="fr-container bg-dsfr-background-alt-blue-france pb-28 pt-4">
-      <ContactIframe className="!h-[60rem] md:!h-[70rem]" />
+    <div className="fr-container  pb-28 pt-4">
+      <ContactIframe className="!h-[79rem] md:!h-[70rem]" />
     </div>
   );
 }
