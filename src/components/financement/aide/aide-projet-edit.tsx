4dd088083148aa2a38593dbe55aa862cf0f31c8d
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-02-27 16:36:09 +0100

    chore: clean code

--- src/components/financement/aide/aide-projet-edit.tsx
+++ src/components/financement/aide/aide-projet-edit.tsx
@@ -1,31 +1,25 @@
 "use client";
 import { TypeFiche } from "@/src/helpers/common";
-import { isEmpty } from "@/src/helpers/listUtils";
 import { PFMV_ROUTES } from "@/src/helpers/routes";
-import { getTypeSolutionFromCode } from "@/src/helpers/type-fiche-solution";
-import { useAideEstimationEditFilter, FichesDiagnosticFiltersKey } from "@/src/hooks/use-aide-estimation-edit-filter";
+import { FichesDiagnosticFiltersKey, useAideEstimationEditFilter } from "@/src/hooks/use-aide-estimation-edit-filter";
 import { useAideEstimationEditSortMethod } from "@/src/hooks/use-aide-estimation-edit-sort-method";
 import { useAidesByProjetFetcher } from "@/src/hooks/use-aides-by-projet-fetcher";
 import { useCanEditProjet } from "@/src/hooks/use-can-edit-projet";
 import { usePagination } from "@/src/hooks/use-pagination";
-import { useImmutableSwrWithFetcher } from "@/src/hooks/use-swr-with-fetcher";
-import { FicheSolution } from "@/src/lib/strapi/types/api/fiche-solution";
 import { useProjetsStore } from "@/src/stores/projets/provider";
 import { useUserStore } from "@/src/stores/user/provider";
-import Tag from "@codegouvfr/react-dsfr/Tag";
-import { memo, useState, useMemo } from "react";
+import { memo, useMemo, useState } from "react";
 import { GenericFicheLink } from "../../common/generic-save-fiche/generic-fiche-link";
 import { getProjetFichesIdsByType } from "../../common/generic-save-fiche/helpers";
-import LinkWithoutPrefetch from "../../common/link-without-prefetch";
 import { notifications } from "../../common/notifications";
 import { Pagination } from "../../common/pagination";
 import BannerProjetBreadcrumb from "../../espace-projet/banner/banner-projet-breadcrumb";
 import { BREADCRUMB_FINANCEMENTS_LISTE } from "../../espace-projet/banner/breadcrumb-list/espace-projet-breadcurmb-financement";
-import { makeFicheSolutionUrlApi } from "../../ficheSolution/helpers";
 import { countAidesByType, countSelectedAides, resolveAidType } from "../helpers";
 import { TypeAidesTerritoiresAide } from "../types";
 import { AideCard } from "./aide-card";
 import { AideCardSkeleton } from "./aide-card-skeleton";
+import { AideSolutionsFilter } from "./aide-solutions-filter";
 import { AideEditFilter } from "./aide-edit-filter";
 import { AideEditSortField } from "./aide-edit-sort-field";
 import { AideProjetListeHeader } from "./aide-projet-liste-header";
@@ -49,10 +43,6 @@ export const AideProjetEdit = memo(() => {
     allFicheSolutionIds.filter((id) => !unselectedFsIds.includes(id)),
   );
 
-  const { data: ficheSolutions } = useImmutableSwrWithFetcher<FicheSolution[]>(
-    allFicheSolutionIds.length > 0 ? makeFicheSolutionUrlApi(allFicheSolutionIds) : "",
-  );
-
   const updateUserProjetInProjet = useProjetsStore((state) => state.updateUserProjetInProjet);
 
   const toggleFicheSolution = (ficheSolutionId: number) => {
@@ -71,7 +61,7 @@ export const AideProjetEdit = memo(() => {
     }
   };
 
-  const { data, isLoading } = useAidesByProjetFetcher(projetId, selectedFsIds);
+  const { data, isLoading } = useAidesByProjetFetcher(projetId, selectedFsIds, true);
   const { aideFinanciereCount, aideTechniqueCount } = countAidesByType(data?.results ?? []);
   const selectedAidesCount = useMemo(
     () => countSelectedAides(data?.results ?? [], projet?.projetAides ?? []),
@@ -129,38 +119,11 @@ export const AideProjetEdit = memo(() => {
         <div className="pfmv-card no-shadow pfmv-card-outline mb-8 w-full p-8">
           <AideProjetPanelHeader />
 
-          {!isEmpty(ficheSolutions) ? (
-            <div className="flex gap-4">
-              <span className="text-nowrap">Solutions :</span>
-
-              <div className="mb-4 flex flex-wrap items-center gap-x-4 gap-y-2">
-                {ficheSolutions?.map((fiche) => (
-                  <Tag
-                    key={fiche.id}
-                    pressed={selectedFsIds.includes(+fiche.id)}
-                    nativeButtonProps={{
-                      onClick: () => toggleFicheSolution(+fiche.id),
-                    }}
-                  >
-                    {getTypeSolutionFromCode(fiche.attributes.type_solution)?.icon("fr-icon--sm mr-1")}{" "}
-                    {fiche.attributes.titre}
-                  </Tag>
-                ))}
-              </div>
-            </div>
-          ) : (
-            <div className="mb-6 flex items-center gap-4">
-              <p className="text-dsfr-text-default-info m-0 text-base">
-                Pour voir des aides plus pertinentes, sélectionnez des solutions dans votre projet
-              </p>
-              <LinkWithoutPrefetch
-                href={PFMV_ROUTES.ESPACE_PROJET_FICHES_SOLUTIONS(projet.id)}
-                className="fr-btn fr-btn--secondary fr-btn--sm shrink-0 rounded-3xl"
-              >
-                Ajouter des solutions
-              </LinkWithoutPrefetch>
-            </div>
-          )}
+          <AideSolutionsFilter
+            allFicheSolutionIds={allFicheSolutionIds}
+            selectedFsIds={selectedFsIds}
+            toggleFicheSolution={toggleFicheSolution}
+          />
 
           <div className="mb-10 flex flex-row items-center justify-between">
             <AideEditFilter
--- src/components/financement/aide/aide-solutions-filter.tsx
+++ src/components/financement/aide/aide-solutions-filter.tsx
@@ -3,51 +3,52 @@ import { PFMV_ROUTES } from "@/src/helpers/routes";
 import { getTypeSolutionFromCode } from "@/src/helpers/type-fiche-solution";
 import { FicheSolution } from "@/src/lib/strapi/types/api/fiche-solution";
 import Tag from "@codegouvfr/react-dsfr/Tag";
-import LinkWithoutPrefetch from "../../common/link-without-prefetch";
+import LinkWithoutPrefetch from "@/src/components/common/link-without-prefetch";
+import { useImmutableSwrWithFetcher } from "@/src/hooks/use-swr-with-fetcher";
+import { makeFicheSolutionUrlApi } from "@/src/components/ficheSolution/helpers";
+import { useProjetsStore } from "@/src/stores/projets/provider";
 
 interface AideSolutionsFilterProps {
-  ficheSolutions?: FicheSolution[];
   selectedFsIds: number[];
   toggleFicheSolution: (ficheSolutionId: number) => void;
-  projetId: number;
+  allFicheSolutionIds: number[];
 }
 
 export const AideSolutionsFilter = ({
-  ficheSolutions,
   selectedFsIds,
   toggleFicheSolution,
-  projetId,
+  allFicheSolutionIds,
 }: AideSolutionsFilterProps) => {
-  if (!isEmpty(ficheSolutions)) {
-    return (
-      <div className="flex gap-4">
-        <span className="text-nowrap">Solutions :</span>
+  const { data: ficheSolutions } = useImmutableSwrWithFetcher<FicheSolution[]>(
+    allFicheSolutionIds.length > 0 ? makeFicheSolutionUrlApi(allFicheSolutionIds) : "",
+  );
+  const currentProjetId = useProjetsStore((state) => state.currentProjetId);
 
-        <div className="mb-4 flex flex-wrap items-center gap-x-4 gap-y-2">
-          {ficheSolutions?.map((fiche) => (
-            <Tag
-              key={fiche.id}
-              pressed={selectedFsIds.includes(+fiche.id)}
-              nativeButtonProps={{
-                onClick: () => toggleFicheSolution(+fiche.id),
-              }}
-            >
-              {getTypeSolutionFromCode(fiche.attributes.type_solution)?.icon("fr-icon--sm mr-1")}{" "}
-              {fiche.attributes.titre}
-            </Tag>
-          ))}
-        </div>
+  return !isEmpty(ficheSolutions) ? (
+    <div className="flex gap-4">
+      <span className="text-nowrap">Solutions :</span>
+
+      <div className="mb-4 flex flex-wrap items-center gap-x-4 gap-y-2">
+        {ficheSolutions?.map((fiche) => (
+          <Tag
+            key={fiche.id}
+            pressed={selectedFsIds.includes(+fiche.id)}
+            nativeButtonProps={{
+              onClick: () => toggleFicheSolution(+fiche.id),
+            }}
+          >
+            {getTypeSolutionFromCode(fiche.attributes.type_solution)?.icon("fr-icon--sm mr-1")} {fiche.attributes.titre}
+          </Tag>
+        ))}
       </div>
-    );
-  }
-
-  return (
+    </div>
+  ) : (
     <div className="mb-6 flex items-center gap-4">
       <p className="text-dsfr-text-default-info m-0 text-base">
         Pour voir des aides plus pertinentes, sélectionnez des solutions dans votre projet
       </p>
       <LinkWithoutPrefetch
-        href={PFMV_ROUTES.ESPACE_PROJET_FICHES_SOLUTIONS(projetId)}
+        href={PFMV_ROUTES.ESPACE_PROJET_FICHES_SOLUTIONS(currentProjetId || 0)}
         className="fr-btn fr-btn--secondary fr-btn--sm shrink-0 rounded-3xl"
       >
         Ajouter des solutions
