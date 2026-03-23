b646ef98a6c8b0301045dce241c88d280ab58533
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-03-23 08:59:08 +0100

    feat: surchauffe urbaine timing page ui

--- src/app/(site-vitrine)/surchauffe-urbaine/quand-faire-un-diagnostic/page.tsx
+++ src/app/(site-vitrine)/surchauffe-urbaine/quand-faire-un-diagnostic/page.tsx
@@ -2,6 +2,9 @@ import { Metadata } from "next";
 import { computeMetadata } from "@/src/helpers/metadata/helpers";
 import { TeaserDiagnosticComponent } from "@/src/components/surchauffe-urbaine/teaser-diagnostic-component";
 import { SurchauffeUrbaineTimingComponent } from "@/src/components/surchauffe-urbaine/surchauffe-urbaine-timing-component";
+import {
+  SurchauffeUrbaineStoriesComponent
+} from "@/src/components/surchauffe-urbaine/surchauffe-urbaine-stories-component";
 
 export const metadata: Metadata = computeMetadata("Pourquoi et quand faire un diagnostic ?");
 
@@ -17,7 +20,7 @@ export default async function SurchauffeUrbaineTimingPage() {
         </div>
       </div>
       <SurchauffeUrbaineTimingComponent className="mt-12" />
-      <TeaserDiagnosticComponent className="mt-12" />
+      <SurchauffeUrbaineStoriesComponent className="mt-12" />
     </>
   );
 }
