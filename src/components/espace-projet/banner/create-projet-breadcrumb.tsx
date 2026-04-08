a3df1a57b9694f5531830ed63cca1d759a69db5c
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-04-08 15:26:52 +0200

    feat: design of creation projet form

---
+++ src/components/espace-projet/banner/create-projet-breadcrumb.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Breadcrumb from "@codegouvfr/react-dsfr/Breadcrumb";
+import { EspaceProjetBreadcrumbStep } from "@/src/components/espace-projet/banner/espace-projet-breadcurmb-list";
+import clsx from "clsx";
+import { useProjetsStore } from "@/src/stores/projets/provider";
+import BannerProjet from "@/src/components/espace-projet/banner/banner-projet";
+
+export default function BannerProjetBreadcrumb({
+  step,
+  className,
+}: {
+  step: EspaceProjetBreadcrumbStep;
+  className?: string;
+}) {
+  const currentProjet = useProjetsStore((state) => state.getCurrentProjet());
+  return (
+    <div className="bg-dsfr-background-alt-blue-france transition-all">
+      <div className="fr-container">
+        {currentProjet && (
+          <Breadcrumb
+            className={clsx(className, "!mb-0 !mt-0 !pb-1 !pt-3")}
+            currentPageLabel={step?.currentPageLabel}
+            classes={{ link: "text-pfmv-navy font-normal" }}
+            segments={step?.breadcrumbSegments(currentProjet.id, currentProjet.nom) || []}
+          />
+        )}
+      </div>
+      <BannerProjet />
+    </div>
+  );
+}
