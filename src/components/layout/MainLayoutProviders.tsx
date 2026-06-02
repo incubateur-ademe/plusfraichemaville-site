7c576222c120bf16a81285259064c53ab58c9969
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-06-02 09:10:05 +0200

    feat: poc post hog survey

--- src/components/layout/MainLayoutProviders.tsx
+++ src/components/layout/MainLayoutProviders.tsx
@@ -9,27 +9,32 @@ import { StartDsfrOnHydration } from "@codegouvfr/react-dsfr/next-app-router";
 import { DsfrProvider } from "@/src/app/dsfr-provider";
 import { GenericSaveModalUnauthenticated } from "@/src/components/common/generic-save-fiche/generic-save-modal-unauthenticated";
 import { UserBrowsingTracker } from "@/src/components/user/UserBrowsingTracker";
+import { PostHogProvider } from "@/src/components/posthog/posthog-provider";
+import { SuspendedPostHogPageView } from "@/src/components/posthog/posthog-page-view";
 
 export default function MainLayoutProviders({ children, lang }: PropsWithChildren<{ lang: string }>) {
   return (
-    <SessionProvider>
-      <UserStoreProvider>
-        <ProjetsStoreProvider>
-          <ModalStoreProvider>
-            <Suspense>
-              <MatomoPageView />
-              <UserBrowsingTracker />
-            </Suspense>
-            <DsfrProvider lang={lang}>
-              <>
-                <StartDsfrOnHydration />
-                {children}
-                <GenericSaveModalUnauthenticated />
-              </>
-            </DsfrProvider>
-          </ModalStoreProvider>
-        </ProjetsStoreProvider>
-      </UserStoreProvider>
-    </SessionProvider>
+    <PostHogProvider>
+      <SessionProvider>
+        <UserStoreProvider>
+          <ProjetsStoreProvider>
+            <ModalStoreProvider>
+              <Suspense>
+                <MatomoPageView />
+                <UserBrowsingTracker />
+                <SuspendedPostHogPageView />
+              </Suspense>
+              <DsfrProvider lang={lang}>
+                <>
+                  <StartDsfrOnHydration />
+                  {children}
+                  <GenericSaveModalUnauthenticated />
+                </>
+              </DsfrProvider>
+            </ModalStoreProvider>
+          </ProjetsStoreProvider>
+        </UserStoreProvider>
+      </SessionProvider>
+    </PostHogProvider>
   );
 }
---
+++ src/components/posthog/posthog-page-view.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { usePostHog } from "posthog-js/react";
+import { useEffect } from "react";
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      const search = searchParams.toString();
+      if (search) {
+        url += `?${search}`;
+      }
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
+
+export function SuspendedPostHogPageView() {
+  return (
+    <PostHogPageView />
+  );
+}
