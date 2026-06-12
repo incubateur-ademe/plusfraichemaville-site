6a057b87451073dff5f9502fb67b02c888b9c5b5
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-06-12 17:22:45 +0200

    feat: put aides territoires call in suspense to not block fiche solution display

--- src/components/ficheSolution/FicheSolutionTabFinancements.tsx
+++ src/components/ficheSolution/FicheSolutionTabFinancements.tsx
@@ -1,22 +1,23 @@
+import { Suspense } from "react";
 import { FicheSolution } from "@/src/lib/strapi/types/api/fiche-solution";
-import { getNbAidesFromAidesTerritoires } from "@/src/lib/aidesTerritoires/fetch";
 import FicheSolutionFinancementsIncentive from "@/src/components/ficheSolution/fiche-solution-financements-incentive";
+import FicheSolutionNbAidesLoader from "@/src/components/ficheSolution/fiche-solution-nb-aides-loader";
 
-export default async function FicheSolutionTabFinancements({
+export default function FicheSolutionTabFinancements({
   ficheId,
   ficheAttributes,
 }: {
   ficheId: number;
   ficheAttributes: FicheSolution["attributes"];
 }) {
-  const nbAides = await getNbAidesFromAidesTerritoires([ficheAttributes]);
-
   return (
     <div className="text-dsfr-text-title-grey">
       <h2 className="mb-8 text-[1.75rem] font-bold">Financements</h2>
 
       <div className="mb-10">
-        <FicheSolutionFinancementsIncentive ficheId={ficheId} nbAides={nbAides} />
+        <Suspense fallback={<FicheSolutionFinancementsIncentive ficheId={ficheId} nbAides={0} />}>
+          <FicheSolutionNbAidesLoader ficheId={ficheId} ficheAttributes={ficheAttributes} />
+        </Suspense>
       </div>
     </div>
   );
