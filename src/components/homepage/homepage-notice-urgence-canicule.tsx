0160d7710f329c34a8656ddca30908855129ad7b
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-07-06 09:07:33 +0200

    feat: ajout de la page urgence canicule

---
+++ src/components/homepage/homepage-notice-urgence-canicule.tsx
@@ -0,0 +1,34 @@
+import Notice from "@codegouvfr/react-dsfr/Notice";
+import LinkWithoutPrefetch from "../common/link-without-prefetch";
+import { PFMV_ROUTES } from "@/src/helpers/routes";
+
+export const HomepageNoticeUrgenceCanicule = () => {
+  return (
+    <Notice
+      classes={{ root: "[&_p]:mb-0" }}
+      title={
+        <>
+          <span className="!mb-0 flex  flex-row align-text-top">
+            <i className="ri-temp-hot-line mr-1"></i>
+            Vague de chaleur, surchauffe urbaine, îlot de chaleur urbain :
+          </span>
+        </>
+      }
+      description={
+        <>
+          <span className="ml-6 inline-block">
+            <LinkWithoutPrefetch
+              className="fr-icon-arrow-right-line fr-link--icon-right"
+              href={PFMV_ROUTES.SURCHAUFFE_URBAINE_REPONDRE_URGENCE}
+            >
+              découvrez les premières actions à mener et comment intégrer le rafraîchissement urbain dans vos projets
+              d'aménagement
+            </LinkWithoutPrefetch>
+          </span>
+        </>
+      }
+      iconDisplayed={false}
+      severity="warning"
+    />
+  );
+};
