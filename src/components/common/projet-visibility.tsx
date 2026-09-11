e50c13808ba3f7e4c5f8c5d680e7cb083382bb9f
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-09-11 17:09:58 +0200

    feat: coquille

--- src/components/common/projet-visibility.tsx
+++ src/components/common/projet-visibility.tsx
@@ -72,7 +72,7 @@ export const ProjetVisibility = ({
         <div className="flex flex-col gap-4 text-sm [&>p]:mb-0">
           {Toggle}
           <p>
-            Grâce à la Carte des projets disponible sur votre espace projet, découvez les initiatives voisines en
+            Grâce à la Carte des projets disponible sur votre espace projet, découvrez les initiatives voisines en
             matière de rafraîchissement urbain, échangez avec d'autres porteurs de projets et obtenez des contacts de
             prestataires. Pour faciliter les échanges entre pairs, nous vous invitons à rendre votre projet visible par
             les autres membres de la communauté Plus fraîche ma ville.
